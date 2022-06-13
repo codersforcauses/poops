619cb52b8bd340aa764564ffb351333e9fd6f1d9
Nicholas Choong <nicholascwc1997@gmail.com>
2022-06-13 14:05:18 +0800

    removed url

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -5,7 +5,7 @@
 module.exports = {
   // !STARTERCONF Change the siteUrl
   /** Without additional '/' on the end, e.g. https://theodorusclarence.com */
-  siteUrl: 'https://tsnext-tw.thcl.dev',
+  siteUrl: '',
   generateRobotsTxt: true,
   robotsTxtOptions: {
     policies: [{ userAgent: '*', allow: '/' }],
