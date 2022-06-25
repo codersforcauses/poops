799a72c8c6c3a6d8fcd29c8eaa52d496821c5296
ChantelleKerr <chantellekerr93@gmail.com>
2022-06-25 15:31:16 +0800

    added mdi dependency and Icon template

--- src/components/navBar/navbar.tsx
+++ src/components/navBar/navbar.tsx
@@ -1,6 +1,16 @@
+import { mdiHome } from '@mdi/js';
+import Icon from '@mdi/react';
+
 const NavBar = () => {
   return (
-    <div>navbar</div>
+    <div>
+      <Icon path={mdiHome}
+        size={1}
+        horizontal
+        vertical
+        rotate={90}
+        color="red"/>
+    </div>
   )
 }
 
