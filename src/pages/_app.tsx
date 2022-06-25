668f4e2023fe2d41f8b46bd109d35ef4b50ab220
ChantelleKerr <chantellekerr93@gmail.com>
2022-06-25 15:08:52 +0800

    Navbar component displayed on homepage

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,6 +3,7 @@ import { AppProps } from 'next/app'
 import '@/styles/globals.css'
 // !STARTERCONF This is for demo purposes, remove @/styles/colors.css import immediately
 import '@/styles/colors.css'
+import NavBar from '@/components/navBar/navbar'
 
 /**
  * !STARTERCONF info
@@ -10,7 +11,12 @@ import '@/styles/colors.css'
  */
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  
+  return (
+    <div>
+      <NavBar/>
+      <Component {...pageProps} />
+    </div>)
 }
 
 export default MyApp
