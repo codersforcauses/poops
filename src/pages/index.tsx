ea8f0f86560449af5ea740c1f5c7e09b838f0816
Nicholas Choong <nicholascwc1997@gmail.com>
2022-06-26 23:38:19 +0800

    added page title

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,22 +1,10 @@
 import Seo from '@/components/Seo'
 
-/**
- * SVGR Support
- * Caveat: No React Props Type.
- *
- * You can override the next-env if the type is important to you
- * @see https://stackoverflow.com/questions/68103844/how-to-override-next-js-svg-module-declaration
- */
-
-// !STARTERCONF -> Select !STARTERCONF and CMD + SHIFT + F
-// Before you begin editing, follow all comments with `STARTERCONF`,
-// to customize the default configuration.
-
 const Home = () => {
   return (
     <>
-      {/* <Seo templateTitle='Home' /> */}
-      <Seo />
+      {/* <Seo /> */}
+      <Seo pageTitle='Home' />
 
       <main>
         <p>Home Page</p>
