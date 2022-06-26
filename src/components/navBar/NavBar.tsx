a128001f8477e9fdea4f2633a2581817547feb2b
pavo-etc <z1verdeman2@gmail.com>
2022-06-26 11:43:19 +0800

    Change to Hero Icons

--- src/components/navBar/NavBar.tsx
+++ src/components/navBar/NavBar.tsx
@@ -1,13 +1,15 @@
 import {
-  mdiClipboardAlertOutline,
-  mdiHomeOutline,
-  mdiMapMarkerOutline,
-  mdiMessageProcessingOutline
-} from '@mdi/js'
+  ChatAltIcon,
+  ExclamationCircleIcon,
+  HomeIcon,
+  LocationMarkerIcon
+} from '@heroicons/react/outline'
 
 import NavLink from '@/components/navBar/NavLink'
 
 export default function NavBar() {
+  const iconClasses = 'inline-block h-7 w-8 hover:text-poops-red'
+
   return (
     <div className='h-14 w-full'>
       <section
@@ -15,10 +17,26 @@ export default function NavBar() {
         className='fixed inset-x-0 bottom-0 z-10 block bg-white shadow'
       >
         <div id='tabs' className='flex justify-between'>
-          <NavLink href='#' name='Home' icon={mdiHomeOutline} />
-          <NavLink href='#' name='Contact' icon={mdiMessageProcessingOutline} />
-          <NavLink href='#' name='Visit' icon={mdiMapMarkerOutline} />
-          <NavLink href='#' name='Incidents' icon={mdiClipboardAlertOutline} />
+          <NavLink
+            href='#'
+            name='Home'
+            icon={<HomeIcon className={iconClasses} />}
+          />
+          <NavLink
+            href='#'
+            name='Contact'
+            icon={<ChatAltIcon className={iconClasses} />}
+          />
+          <NavLink
+            href='#'
+            name='Visit'
+            icon={<LocationMarkerIcon className={iconClasses} />}
+          />
+          <NavLink
+            href='#'
+            name='Incidents'
+            icon={<ExclamationCircleIcon className={iconClasses} />}
+          />
         </div>
       </section>
     </div>
--- src/components/navBar/NavLink.tsx
+++ src/components/navBar/NavLink.tsx
@@ -1,32 +1,16 @@
-import { useState } from 'react'
-import Icon from '@mdi/react'
-
 type NavLinkProps = {
   href: string
   name: string
-  icon: string
+  icon: JSX.Element
 }
 
 export default function NavLink({ href, name, icon }: NavLinkProps) {
-  const [isHighlighted, setIsHighlighted] = useState(false)
-
-  const color = isHighlighted ? 'var(--poops-red)' : 'black'
-
   return (
     <a
       href={href}
       className='inline-block w-full justify-center pt-2 pb-1 text-center hover:text-poops-red'
-      onMouseEnter={() => setIsHighlighted(true)}
-      onMouseLeave={() => setIsHighlighted(false)}
     >
-      <Icon
-        className='mb-1 inline-block'
-        path={icon}
-        size={1}
-        vertical={false}
-        color={color}
-      />
-
+      {icon}
       <span className='tab tab-home block text-xs'>{name}</span>
     </a>
   )
