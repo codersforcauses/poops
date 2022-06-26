d7b6278f8c56b0d286bc144a9517f2cd6988587f
pavo-etc <z1verdeman2@gmail.com>
2022-06-26 12:57:31 +0800

    Add tab glow for selected tab

--- src/components/navBar/NavBar.tsx
+++ src/components/navBar/NavBar.tsx
@@ -1,3 +1,4 @@
+import { useState } from 'react'
 import {
   ChatAltIcon,
   ExclamationCircleIcon,
@@ -8,35 +9,49 @@ import {
 import NavLink from '@/components/navBar/NavLink'
 
 export default function NavBar() {
+  const [currentPage, setCurrentPage] = useState('Home')
   const iconClasses = 'inline-block h-7 w-8 hover:text-poops-red'
 
+  const linkDetails = [
+    { name: 'Home', href: '#', icon: <HomeIcon className={iconClasses} /> },
+    {
+      name: 'Contact',
+      href: '#',
+      icon: <ChatAltIcon className={iconClasses} />
+    },
+    {
+      name: 'Visit',
+      href: '#',
+      icon: <LocationMarkerIcon className={iconClasses} />
+    },
+    {
+      name: 'Incidents',
+      href: '#',
+      icon: <ExclamationCircleIcon className={iconClasses} />
+    }
+  ]
+
+  const tabs = linkDetails.map((link) => {
+    return (
+      <NavLink
+        href={link.href}
+        name={link.name}
+        key={link.name}
+        currentPage={link.name === currentPage}
+        setCurrentPage={setCurrentPage}
+        icon={link.icon}
+      />
+    )
+  })
+
   return (
-    <div className='h-14 w-full'>
+    <div className='h-16 w-full'>
       <section
         id='bottom-navigation'
         className='fixed inset-x-0 bottom-0 z-10 block bg-white shadow'
       >
         <div id='tabs' className='flex justify-between'>
-          <NavLink
-            href='#'
-            name='Home'
-            icon={<HomeIcon className={iconClasses} />}
-          />
-          <NavLink
-            href='#'
-            name='Contact'
-            icon={<ChatAltIcon className={iconClasses} />}
-          />
-          <NavLink
-            href='#'
-            name='Visit'
-            icon={<LocationMarkerIcon className={iconClasses} />}
-          />
-          <NavLink
-            href='#'
-            name='Incidents'
-            icon={<ExclamationCircleIcon className={iconClasses} />}
-          />
+          {tabs}
         </div>
       </section>
     </div>
--- src/components/navBar/NavLink.tsx
+++ src/components/navBar/NavLink.tsx
@@ -2,13 +2,25 @@ type NavLinkProps = {
   href: string
   name: string
   icon: JSX.Element
+  currentPage: boolean
+  setCurrentPage: (name: string) => unknown
 }
 
-export default function NavLink({ href, name, icon }: NavLinkProps) {
+export default function NavLink({
+  href,
+  name,
+  icon,
+  currentPage,
+  setCurrentPage
+}: NavLinkProps) {
+  const tabClasses =
+    'inline-block w-full justify-center pt-2 pb-1 text-center hover:text-poops-red'
+  const currentTabClasses = tabClasses + ' border-2 border-t-poops-red'
   return (
     <a
       href={href}
-      className='inline-block w-full justify-center pt-2 pb-1 text-center hover:text-poops-red'
+      className={currentPage ? currentTabClasses : tabClasses}
+      onClick={() => setCurrentPage(name)}
     >
       {icon}
       <span className='tab tab-home block text-xs'>{name}</span>
