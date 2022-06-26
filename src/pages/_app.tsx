beae0cfb4c2de450485fc3fd3e6086f82cef8c71
Nicholas Choong <nicholascwc1997@gmail.com>
2022-06-26 23:46:58 +0800

    removed comments

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,11 +4,6 @@ import NavBar from '@/components/NavBar'
 
 import '@/styles/globals.css'
 
-/**
- * !STARTERCONF info
- * ? `Layout` component is called in every page using `np` snippets. If you have consistent layout across all page, you can add it here too
- */
-
 const POOPS = ({ Component, pageProps }: AppProps) => {
   return (
     <>
