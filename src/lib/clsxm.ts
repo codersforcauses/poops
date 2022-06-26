3e646e16dd338b63923ef553e1eba981bd8b9a4c
Nicholas Choong <nicholascwc1997@gmail.com>
2022-06-26 23:23:04 +0800

    removed bloats

--- src/lib/clsxm.ts
+++
@@ -1,9 +0,0 @@
-import clsx, { ClassValue } from 'clsx'
-import { twMerge } from 'tailwind-merge'
-
-/** Merge classes with tailwind-merge with clsx full feature */
-const clsxm = (...classes: ClassValue[]) => {
-  return twMerge(clsx(...classes))
-}
-
-export default clsxm
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,36 +1,26 @@
-import Document, {
-  DocumentContext,
-  Head,
-  Html,
-  Main,
-  NextScript
-} from 'next/document'
+import { Head, Html, Main, NextScript } from 'next/document'
 
-class MyDocument extends Document {
-  static async getInitialProps(ctx: DocumentContext) {
-    const initialProps = await Document.getInitialProps(ctx)
-    return { ...initialProps }
-  }
+import Favicon from '@/components/Seo/Favicon'
 
-  render() {
-    return (
-      <Html lang='en'>
-        <Head>
-          <link
-            rel='preload'
-            href='/fonts/inter-var-latin.woff2'
-            as='font'
-            type='font/woff2'
-            crossOrigin='anonymous'
-          />
-        </Head>
-        <body>
-          <Main />
-          <NextScript />
-        </body>
-      </Html>
-    )
-  }
+const Document = () => {
+  return (
+    <Html lang='en'>
+      <Head>
+        <link
+          rel='preload'
+          href='/fonts/inter-var-latin.woff2'
+          as='font'
+          type='font/woff2'
+          crossOrigin='anonymous'
+        />
+        <Favicon />
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  )
 }
 
-export default MyDocument
+export default Document
