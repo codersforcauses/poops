e9fcb01a811521c8f01dd34d8c71bdc5580829d1
Dylan To <dylan.to@hotmail.com>
2022-06-28 01:42:56 +0800

    Make navbar lie ontop of content

--- src/components/NavBar/index.tsx
+++ src/components/NavBar/index.tsx
@@ -46,7 +46,7 @@ export default function NavBar() {
   })
 
   return (
-    <div className='h-16 w-full justify-center'>
+    <div className='fixed h-16 w-full'>
       <nav
         id='bottom-navigation'
         className='fixed inset-x-0 bottom-0 z-10 block bg-white shadow'
