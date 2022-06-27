413828d04dadd6a184eb6c41949084fe4a32b1e5
ChantelleKerr <chantellekerr93@gmail.com>
2022-06-27 22:44:20 +0800

    Added profile page template

---
+++ src/pages/profile.tsx
@@ -0,0 +1,16 @@
+import Seo from '@/components/Seo'
+
+const Profile = () => {
+  return (
+    <>
+      {/* <Seo /> */}
+      <Seo pageTitle='Profile' />
+
+      <main>
+        <p>Profile Page</p>
+      </main>
+    </>
+  )
+}
+
+export default Profile
