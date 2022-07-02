c946d3b75611a20dbdbc9d6ce682e7ae51473d14
Jake Lyell <me@jakelyell.dev>
2022-07-02 12:33:06 +0800

    linting fix

--- src/context/AuthContext.tsx
+++ src/context/AuthContext.tsx
@@ -1,6 +1,13 @@
 import React, { createContext, useContext, useEffect, useState } from 'react'
-import { onAuthStateChanged } from 'firebase/auth'
-import { User } from 'firebase/auth'
+import {
+  Auth,
+  getRedirectResult,
+  GoogleAuthProvider,
+  onAuthStateChanged,
+  signInWithRedirect,
+  signOut,
+  User
+} from 'firebase/auth'
 
 import { auth } from '../components/Firebase/init'
 
@@ -16,9 +23,58 @@ export const AuthContextProvider = ({
   const [currentUser, setCurrentUser] = useState<User | null>(null)
   const [loading, setLoading] = useState(true)
 
-  // function signup(email: string, password: string) { // CURRENTLY NOT IN USE, COULD BE USED IF SCOPE CHANGES
-  //   return createUserWithEmailAndPassword(auth, email, password)
-  // }
+  function googleSignIn(auth: Auth) {
+    // TODO add error checking
+    const googleProvider = new GoogleAuthProvider()
+    // return signInWithRedirect(auth, googleProvider);
+    signInWithRedirect(auth, googleProvider)
+      .then((result) => {
+        // console.log(result);
+        return result
+      })
+      .catch((error) => {
+        // console.log(error);
+        return error
+      })
+  }
+
+  function getGoogleResults(auth: Auth) {
+    getRedirectResult(auth)
+      .then((result) => {
+        if (result !== null) {
+          const credential = GoogleAuthProvider.credentialFromResult(result)
+          if (credential !== null) {
+            // const token = credential.accessToken;
+          }
+          const user = result.user
+          return user
+        }
+      })
+      .catch((error) => {
+        return error
+        // console.log(error);
+        // const errorCode = error.code;
+        // const errorMessage = error.message;
+        // The email of the user's account used.
+        // const email = error.customData.email;
+        // The AuthCredential type that was used.
+        // const credential = GoogleAuthProvider.credentialFromError(error);
+      })
+  }
+
+  function logOut() {
+    // TODO add error checking
+    // return signOut(auth);
+    signOut(auth)
+      .then((result) => {
+        // console.log(result)
+        return result
+      })
+      .catch((error) => {
+        // console.log(error)
+        return error
+      })
+  }
 
   useEffect(() => {
     const unsubscribe = onAuthStateChanged(auth, (user) => {
@@ -29,8 +85,10 @@ export const AuthContextProvider = ({
   }, [])
 
   const value = {
-    currentUser
-    // signup,
+    currentUser,
+    googleSignIn,
+    getGoogleResults,
+    logOut
   }
 
   return (
