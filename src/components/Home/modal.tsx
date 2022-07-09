2ee3d36b1100780344f5fcf74c732f9aa686611e
chayG5 <chaygango5@gmail.com>
2022-07-09 15:02:59 +0800

    multi select drop down, installed react select

--- src/components/Home/modal.tsx
+++ src/components/Home/modal.tsx
@@ -1,23 +1,44 @@
 import React from 'react'
 import { useState} from 'react'
+import { default as ReactSelect } from "react-select"
+import { components } from "react-select"
 import Image from 'next/image'
 
-
-// interface Mode {
-//   start: Number
-//   stop: Number
-// }
+const Option = (props) => {
+  return (
+    <div>
+      <components.Option {...props}>
+        <input
+          type="checkbox"
+          checked={props.isSelected}
+          onChange={() => null}
+        />{" "}
+        <label>{props.label}</label>
+      </components.Option>
+    </div>
+  );
+};
 
 
 function Modal() {
-  // var total = 0
-  // if (props.stop > 0){
-  //   total = props.stop - props.start
-  // }
+
   const [modalIsOpen, setModalOpen] = useState(false)
   const [type, setType] = useState("TYPE OF VISIT")
   const [pet, setPet] = useState("SELECT PET")
   const [distance, setDistance] = useState(0)
+  const [optionSelected, setSelected] = useState("")
+
+  const pets = [
+    { value: 'Willow', label: 'Willow' },
+    { value: 'Nala', label: 'Nala' },
+    { value: 'Coco', label: 'Coco' },
+    { value: 'Nigi', label: 'Nigi' }
+  ]
+
+  const handleChange = (selected) => {
+    setSelected(selected)
+  };
+  
   return (
     <div>
       <div className='text-center'>
@@ -46,24 +67,26 @@ function Modal() {
       />
       <br />
       <form style={{ fontSize: 18 }}>
-        {/* <label htmlFor='start'>Start : </label>
-        <input type='text' id='start' name='start' value={props.start}/>
-        <br />
-        <label htmlFor='stop'>End : </label>  
-        <input type='text' id='stop' name='stop' value={props.stop}></input>
-        <br />
-        <label htmlFor='total'>Total time : </label>
-        <input type='text' id='total' name='total' value={total}></input>
-        <br /> */}
-        <select id='pets' name='pets' onChange={e => setPet(e.target.value)}>
+        {/* <select id='pets' name='pets' onChange={e => setPet(e.target.value)}>
         <option value='SELECT PET'>SELECT PET</option>
           <option value='Willow'>Willow</option>
           <option value='Nala'>Nala</option>
           <option value='Coco'>Coco</option>
           <option value='Gigi'>Gigi</option>
-        </select>
+        </select> */}
+        <ReactSelect
+          options={pets}
+          isMulti
+          closeMenuOnSelect={false}
+          hideSelectedOptions={false}
+          components={{
+            Option
+          }}
+          onChange={handleChange}
+          value={optionSelected}
+        />  
         <br></br>
-        <br></br>
+        <br></br>   
         <select id='type' name='type' onChange={e => setType(e.target.value)}>
           <option value='TYPE OF VISIT'>TYPE OF VISIT</option>
           <option value='Vet'>Vet</option>
@@ -75,11 +98,11 @@ function Modal() {
     </div> 
     <br></br>
     <div className='text-center'>
-    {(((type == "Vet" || type == "Transportation") && pet != "SELECT PET" && distance == 0 && modalIsOpen) || (type == "Walk" && pet != "SELECT PET" && distance > 0 && modalIsOpen)) && (
+    {(((type == "Vet" || type == "Transportation") && optionSelected && distance == 0 && modalIsOpen) || (type == "Walk" && optionSelected && distance > 0 && modalIsOpen)) && (
           <button
             className='relative h-[37px] w-[150px] rounded-lg bg-poops-dark-red text-xl font-semibold text-white'
             onClick={() => {
-              setModalOpen(false)
+              setModalOpen(false), setType("TYPE OF VISIT"), setPet("SELECT PET"), setDistance(0)
             }}
           >
             STOP VISIT
