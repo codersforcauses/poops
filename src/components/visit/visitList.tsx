19243f6088d8e4428e9f7bb2c8b4792cac0618c0
Dustybluelight <104851317+Dustybluelight@users.noreply.github.com>
2022-07-10 11:22:58 +0800

    refac: adjusted visitList to follow PR recommendations

--- src/components/visit/visitList.tsx
+++ src/components/visit/visitList.tsx
@@ -11,27 +11,27 @@ export default function VisitList(props: VisitListProps) {
   const [visitData, setVisitData] = useState<Visit[]>([])
 
   useEffect(() => {
-    const fetchData = async () => {
-      const response: Visit[] = await getVisitData()
-      setVisitData(response)
-    }
+    const fetchData = () => getVisitData().then(setVisitData)
     fetchData()
-  }, [])
+  })
+
+  const matchesFirstName = (post: Visit) =>
+    post.firstName.toLowerCase().includes(props.searchQuery.toLowerCase())
+
+  const matchesLastName = (post: Visit) =>
+    post.lastName.toLowerCase().includes(props.searchQuery.toLowerCase())
+
+  const matchesPetName = (post: Visit) =>
+    post.petName.toLowerCase().includes(props.searchQuery.toLowerCase())
+
+  const matchesSearchTerms = (post: Visit) =>
+    matchesFirstName(post) || matchesLastName(post) || matchesPetName(post)
 
   return (
     <div className=''>
       {visitData
         .filter((post: Visit) => {
-          if (
-            props.searchQuery === '' ||
-            post.firstName
-              .toLowerCase()
-              .includes(props.searchQuery.toLowerCase()) ||
-            post.lastName
-              .toLowerCase()
-              .includes(props.searchQuery.toLowerCase()) ||
-            post.petName.toLowerCase().includes(props.searchQuery.toLowerCase())
-          ) {
+          if (props.searchQuery === '' || matchesSearchTerms(post)) {
             return post
           }
         })
