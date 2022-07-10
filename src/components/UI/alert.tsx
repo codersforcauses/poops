67600f3340e41778b0e48f9fca0653a4abe1f445
swolrus <david@dwn.dev>
2022-07-10 15:17:35 +0800

    convert to heroicon components

--- src/components/UI/alert.tsx
+++ src/components/UI/alert.tsx
@@ -1,4 +1,5 @@
 import { useState, useRef, useEffect } from 'react'
+import { InformationCircleIcon, ShieldExclamationIcon, ExclamationIcon, AnnotationIcon, XIcon } from '@heroicons/react/outline'
 
 const iconColor = '#000000'
 const titleColor = '#000000'
@@ -12,16 +13,17 @@ const shadow = '0 2px 4px rgba(0,0,0,0.1),\
                 0 64px 128px rgba(0,0,0,0.1)'
 
 const getClasses = (visible: boolean, className: string) => {
-  let pos = 'flex items-center fixed top-0 left-0 right-0 z-999 pl-6 pt-3 pr-3 pb-2 mx-[2rem] '
-  let design = 'bg-white rounded-b-md text-black border-x-1 border-b-1 border-[#dddddd] transition-transform duration-300 delay-150 '
-  let animate = visible ? 'translate-y-0 ease-out' : '-translate-y-[200%] ease-in'
-  return (pos + design + animate + className)
+  return ('flex items-center fixed top-0 left-0 right-0 z-999 pl-6 pt-4 pr-3 pb-3 mx-[2rem] \
+              bg-white rounded-b-md text-black border-x-1 border-b-1 border-[#dddddd] \
+              transition-transform duration-300 delay-150 ' +
+            (visible ? 'translate-y-0 ease-out ' : '-translate-y-[200%] ease-in ') + className)
 }
 
 enum ModalType {
   'info',
+  'security',
   'problem',
-  'comment'
+  'comment',
 }
 
 type AlertProps = {
@@ -74,31 +76,25 @@ const Alert: React.FC<AlertProps> = ({
       >
         <div className='w-6 h-6'>
         { variant === 'info' ? (
-            <svg xmlns='http://www.w3.org/2000/svg' className='h-6 w-6' fill='none' viewBox='0 0 24 24' stroke={iconColor} strokeWidth={2}>
-              <path strokeLinecap='round' strokeLinejoin='round' d='M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z' />
-            </svg>
+            <InformationCircleIcon className='h-6 w-6'/>
+          ) : variant === 'security' ? (
+            <ShieldExclamationIcon className='h-6 w-6'/>
           ) : variant === 'problem' ? (
-            <svg xmlns='http://www.w3.org/2000/svg' className='h-6 w-6' fill='none' viewBox='0 0 24 24' stroke={iconColor} strokeWidth={2}>
-              <path strokeLinecap='round' strokeLinejoin='round' d='M12 9v2m0 4h.01m-6.938 4h13.856c1.54 0 2.502-1.667 1.732-3L13.732 4c-.77-1.333-2.694-1.333-3.464 0L3.34 16c-.77 1.333.192 3 1.732 3z' />
-            </svg>
+            <ExclamationIcon className='h-6 w-6'/>
           ) : variant === 'comment' && (
-            <svg xmlns='http://www.w3.org/2000/svg' className='h-6 w-6' fill='none' viewBox='0 0 24 24' stroke={iconColor} strokeWidth={2}>
-              <path strokeLinecap='round' strokeLinejoin='round' d='M7 8h10M7 12h4m1 8l-4-4H5a2 2 0 01-2-2V6a2 2 0 012-2h14a2 2 0 012 2v8a2 2 0 01-2 2h-3l-4 4z' />
-            </svg>
+            <AnnotationIcon className='h-6 w-6'/>
           )
         }
         </div>
         <p className='text-xl font-bold ml-[2rem] leading-4' style={{color: titleColor}}>Title</p>
-        <p className='grow ml-4 mr-[2rem]' style={{color: textColor}}>{ currentText }</p>
+        <p className='self-start grow ml-4 mr-[2rem]' style={{color: textColor}}>{ currentText }</p>
         <button
         className='self-start'
         onClick={() => {
           setVisible(false)
           setText('')
         }}>
-          <svg xmlns='http://www.w3.org/2000/svg' className='h-4 w-4' fill='none' viewBox='0 0 24 24' stroke={iconColor} strokeWidth={2}>
-            <path strokeLinecap='round' strokeLinejoin='round' d='M6 18L18 6M6 6l12 12' />
-          </svg>
+          <XIcon className='h-5 w-5' stroke={iconColor} />
         </button>
       </div>
     )
