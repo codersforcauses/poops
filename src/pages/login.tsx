f91fac1b8c5d356ef5dd727154add5edaa1ae792
Nicholas Choong <nicholascwc1997@gmail.com>
2022-07-14 00:42:25 +0800

    removed fragment and added name and logout btn

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -2,41 +2,50 @@ import { NextPage } from 'next'
 import Image from 'next/image'
 
 import LoginPanel from '@/components/Login/LoginPanel'
+import { useAuth } from '@/context/AuthContext'
 
 const Login: NextPage = () => {
+  const { logOut, currentUser } = useAuth()
   return (
-    <>
-      <main>
-        <title>Login</title>
-        <div className='animate-text bg-gradient-to-b from-zinc-300 via-zinc-200 to-zinc-50 '>
-          <div className='m-auto max-w-sm p-10'>
-            <Image
-              src='/images/poops-logo-transparent.png'
-              width={36}
-              height={36}
-              layout='responsive'
-              alt='POOPS logo'
-              className='rounded-full'
-            ></Image>
-          </div>
-
-          <div className='p-3 text-center text-xl font-bold'>Sign In</div>
-
-          <div className='text-x1 text-center font-sans'>
-            Use any one of your profiles
-          </div>
-
-          <LoginPanel
-            displayGoogle={true}
-            displayFacebook={true}
-            displayTwitter={true}
-            displayMicrosoft={true}
-            displayYahoo={false}
-            displayApple={false}
-          />
+    <main>
+      <title>Login</title>
+      <div className='animate-text bg-gradient-to-b from-zinc-300 via-zinc-200 to-zinc-50 '>
+        <div className='m-auto max-w-sm p-10'>
+          <Image
+            src='/images/poops-logo-transparent.png'
+            width={36}
+            height={36}
+            layout='responsive'
+            alt='POOPS logo'
+            className='rounded-full'
+          ></Image>
         </div>
-      </main>
-    </>
+
+        <div className='p-3 text-center text-xl font-bold'>Sign In</div>
+
+        <div className='text-x1 text-center font-sans'>
+          Use any one of your profiles
+        </div>
+
+        <LoginPanel
+          displayGoogle={true}
+          displayFacebook={true}
+          displayTwitter={true}
+          displayMicrosoft={true}
+          displayYahoo={false}
+          displayApple={false}
+        />
+      </div>
+      {/* //! used for testing} */}
+      <br />
+      <br />
+      {currentUser && (
+        <div className='text-center'>
+          <p>{currentUser?.displayName}</p>
+          <button onClick={() => logOut?.()}>logout</button>
+        </div>
+      )}
+    </main>
   )
 }
 
