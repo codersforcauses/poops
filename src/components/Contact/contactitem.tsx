982392b57a5422df71638520801c4f5315aaeb8b
James van de Ven <hello@jamesvandeven.com>
2022-07-16 15:18:42 +0800

    Remove code from other branch

--- src/components/Contact/contactitem.tsx
+++ src/components/Contact/contactitem.tsx
@@ -7,22 +7,17 @@ type ContactItemProps = {
 }
 
 const ContactItem = ({ contact, image }: ContactItemProps) => {
-  const petNames = contact.pets
-    .map((pet) => {
-      return pet.name
-    })
-    .join(', ')
-
   return (
-    <li className='flex items-center justify-between truncate border-b border-gray-300 bg-white p-3 px-5 text-sm text-gray-900 hover:bg-gray-300 focus:bg-gray-300 sm:py-4'>
-      {/* USER PROFILE IMAGE */}
-      <span className='flex items-center space-x-4'>
+    <li className='border-b border-gray-300 bg-white p-3 px-5 hover:bg-gray-300 focus:bg-gray-300 sm:py-4'>
+      <div className='flex items-center space-x-4'>
+        {/* USER PROFILE IMAGE */}
         <Avatar image={image} height={40} width={40} iconClass='h-10 w-10' />
-        <p className='font-medium text-gray-700'>
-          {`${contact.firstName} ${contact.lastName}`}
-        </p>
-      </span>
-      <p className=''>{petNames}</p>
+        <div className='min-w-0 flex-1'>
+          <p className='truncate text-sm font-medium text-gray-900'>
+            {`${contact.firstName} ${contact.lastName}`}
+          </p>
+        </div>
+      </div>
     </li>
   )
 }
