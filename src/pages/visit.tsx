ac717896cc820f7ddc44930c7019406ed8a54d3d
swolrus <david@dwn.dev>
2022-07-17 16:36:29 +0800

    remove testing from visit

--- src/pages/visit.tsx
+++ src/pages/visit.tsx
@@ -1,15 +1,12 @@
 import Header from '@/components/Header'
 import NavBar from '@/components/NavBar'
 // import { withProtected } from '@/components/PrivateRoute'
-//
-import AlertExamples from '@/components/UI/alertExample'
 
 const Visit = () => {
   return (
     <>
       {/* <Header /> */}
       <Header pageTitle='Visit' />
-      <AlertExamples />
       <main>
         <p>Visit Page</p>
       </main>
