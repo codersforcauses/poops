fb28bfbcd82b0ba2176a1d291895cba26d99c4c4
skyheat <6574274+skyheat@users.noreply.github.com>
2022-07-18 18:03:26 +0800

    Cypress setup and initial tests

---
+++ cypress/e2e/spec.cy.ts
@@ -0,0 +1,12 @@
+describe('Launch website', () => {
+  it('visits the website', () => {
+    cy.visit('http://localhost:3000')
+  })
+})
+
+describe('Contact test', () => {
+  it('clicking "Contact" navigates to the contact page', () => {
+    cy.contains('Contact').click()
+    cy.url().should('include', '/contact')
+  })
+})
