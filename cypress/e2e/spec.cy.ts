55cf062f8d610a8f426dd01ff889ad4aaa4e851b
skyheat <6574274+skyheat@users.noreply.github.com>
2022-07-19 22:49:19 +0800

    linting errors fix

--- cypress/e2e/spec.cy.ts
+++ cypress/e2e/spec.cy.ts
@@ -26,12 +26,10 @@ describe('Click edit', () => {
       //.find('path[d="M15.232 5.232l3.536 3.536m-2.036-5.036a2.5 2.5 0 113.536 3.536L6.5 21.036H3v-3.572L16.732 3.732z"]')
       .find('path')
       .first()
-      .click({force: true})
-    
+      .click({ force: true })
 
     cy.url().should('include', '/me')
   })
 })
-
 
 export {}
