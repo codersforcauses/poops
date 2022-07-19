885a0a884f79c707c436e3376e6dcd8f295fc934
skyheat <6574274+skyheat@users.noreply.github.com>
2022-07-19 15:56:41 +0800

    Added contacts page test and edit contacts test

--- cypress/e2e/spec.cy.ts
+++ cypress/e2e/spec.cy.ts
@@ -11,4 +11,27 @@ describe('Contact test', () => {
   })
 })
 
+describe('Click Me', () => {
+  it('clicking "Me" navigates to the user page', () => {
+    cy.contains('Me').click()
+    cy.url().should('include', '/me')
+    //cy.contains('Back').click()
+  })
+})
+
+describe('Click edit', () => {
+  it('clicking "edit" button allows for editing of the profile page', () => {
+    cy.get('svg')
+      .should('have.class', 'm-auto flex h-7 w-7 cursor-pointer')
+      //.find('path[d="M15.232 5.232l3.536 3.536m-2.036-5.036a2.5 2.5 0 113.536 3.536L6.5 21.036H3v-3.572L16.732 3.732z"]')
+      .find('path')
+      .first()
+      .click({force: true})
+    
+
+    cy.url().should('include', '/me')
+  })
+})
+
+
 export {}
