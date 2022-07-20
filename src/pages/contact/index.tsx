fe85deaf586c7a302b4f235438490921c2dc9c79
yafavedave <dav.vivid@icloud.com>
2022-07-20 16:32:51 +0800

    namefix

--- src/pages/contact/index.tsx
+++ src/pages/contact/index.tsx
@@ -8,7 +8,7 @@ import ProfileItem from '@/components/Contact/profileitem'
 import Header from '@/components/Header'
 import NavBar from '@/components/NavBar'
 import SearchBar from '@/components/SearchBar'
-import SearchTag from '@/components/SearchBar/searchTag'
+import SearchTag from '@/components/SearchBar'
 import type { Contact } from '@/types/types'
 // import { withProtected } from '@/components/PrivateRoute
 
