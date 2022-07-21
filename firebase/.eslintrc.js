341d29686eeb8fd29376af8870da2e75b393c553
Nicholas Choong <nicholascwc1997@gmail.com>
2022-07-21 23:23:01 +0800

    init ts

--- firebase/.eslintrc.js
+++ firebase/.eslintrc.js
@@ -4,9 +4,29 @@ module.exports = {
     es6: true,
     node: true,
   },
-  engines: {node: "16"},
-  extends: ["eslint:recommended", "google"],
+  extends: [
+    "eslint:recommended",
+    "plugin:import/errors",
+    "plugin:import/warnings",
+    "plugin:import/typescript",
+    "google",
+    "plugin:@typescript-eslint/recommended",
+  ],
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    tsconfigRootDir: __dirname,
+    project: ["tsconfig.json", "tsconfig.dev.json"],
+    sourceType: "module",
+  },
+  ignorePatterns: [
+    "/lib/**/*", // Ignore built files.
+  ],
+  plugins: [
+    "@typescript-eslint",
+    "import",
+  ],
   rules: {
-    quotes: ["error", "double"],
+    "quotes": ["error", "double"],
+    "import/no-unresolved": 0,
   },
 };
--- firebase/functions/index.ts
+++ firebase/functions/index.ts
@@ -1,50 +1,42 @@
-// @ts-nocheck
-const functions = require("firebase-functions");
-const admin = require("firebase-admin");
-admin.initializeApp(functions.config().firebase);
+import {config, region, logger} from "firebase-functions";
+import {initializeApp} from "firebase-admin/app";
+initializeApp(config().firebase);
+// import * as admin from "firebase-admin";
+// admin.initializeApp(config().firebase);
 
-const { PubSub } = require("@google-cloud/pubsub");
+import {PubSub} from "@google-cloud/pubsub";
 const pubsub = new PubSub();
 
-exports.pubsubWriter = functions
-  .region("australia-southeast1")
-  .https.onRequest( async (req, res) => {
-    console.log("Pubsub Emulator:", process.env.PUBSUB_EMULATOR_HOST);
-    
-    const msg = await pubsub.topic('billing').publishJSON({
-      budgetDisplayName: "name-of-budget",
-      alertThresholdExceeded: 1.0,
-      costAmount: 100.01,
-      costIntervalStart: "2019-01-01T00:00:00Z",
-      budgetAmount: 0.2,
-      budgetAmountType: "SPECIFIED_AMOUNT",
-      currencyCode: "AUD"
-    }, { attr1: 'value' });
+export const pubsubWriter = region("australia-southeast1")
+    .https.onRequest( async (req, res) => {
+      console.log("Pubsub Emulator:", process.env.PUBSUB_EMULATOR_HOST);
 
-    res.json({
-      published: msg
-    })
-  });
+      const msg = await pubsub.topic("billing").publishJSON({
+        budgetDisplayName: "name-of-budget",
+        alertThresholdExceeded: 1.0,
+        costAmount: 100.01,
+        costIntervalStart: "2019-01-01T00:00:00Z",
+        budgetAmount: 0.2,
+        budgetAmountType: "SPECIFIED_AMOUNT",
+        currencyCode: "AUD",
+      }, {attr1: "value"});
 
-// Create and Deploy Your First Cloud Functions
-// https://firebase.google.com/docs/functions/write-firebase-functions
-
-// GET REST API:
-// http://localhost:5001/poops-9dbf6/australia-southeast1/helloWorld
-
-exports.helloWorld = functions
-    .region("australia-southeast1")
-    .https.onRequest((request, response) => {
-      functions.logger.info("Hello logs!", {structuredData: true});
-      response.send("Hello from Firebase!");
+      res.json({
+        published: msg,
+      });
     });
 
+export const helloWorld = region("australia-southeast1")
+    .https.onRequest((request, response) => {
+      logger.info("Hello logs!", {structuredData: true});
+      response.send("Hello from Firebase!");
+    });
 
 
 // ######################################################################
 // Billing Killswitch
 
-const {CloudBillingClient} = require("@google-cloud/billing");
+import {CloudBillingClient} from "@google-cloud/billing";
 const billing = new CloudBillingClient();
 
 const PROJECT_ID = "poops-9dbf6";
@@ -55,7 +47,7 @@ const PROJECT_NAME = `projects/${PROJECT_ID}`;
  * @param {string} projectName Name of project to check if billing is enabled
  * @return {Promise<boolean>} Whether project has billing enabled or not
  */
- const _isBillingEnabled = async (projectName) => {
+const _isBillingEnabled = async (projectName:string) => {
   try {
     const [res] = await billing.getProjectBillingInfo({name: projectName});
     console.log("isBillingEnabled:", res.billingEnabled);
@@ -67,14 +59,14 @@ const PROJECT_NAME = `projects/${PROJECT_ID}`;
     );
     return true;
   }
- };
+};
 
- /**
+/**
  * Disable billing for a project by removing its billing account
  * @param {string} projectName Name of project disable billing on
  * @return {Promise<string>} Text containing response from disabling billing
  */
-const _disableBillingForProject = async (projectName) => {
+const _disableBillingForProject = async (projectName:string) => {
   console.log("Disabling billing");
   const [res] = await billing.updateProjectBillingInfo({
     name: projectName,
@@ -84,8 +76,7 @@ const _disableBillingForProject = async (projectName) => {
   return `Billing disabled: ${JSON.stringify(res)}`;
 };
 
-exports.stopBilling = functions
-    .region("australia-southeast1")
+export const stopBilling = region("australia-southeast1")
     .pubsub.topic("billing")
     .onPublish(async (pubsubEvent) => {
       const pubsubData = JSON.parse(
