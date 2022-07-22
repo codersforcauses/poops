6d2d9d4d8dbd2140a7b8af144912376d29542a08
Nicholas Choong <nicholascwc1997@gmail.com>
2022-07-22 22:58:04 +0800

    only runs on client side, added offline & emulator

--- src/components/Firebase/init.ts
+++ src/components/Firebase/init.ts
@@ -1,6 +1,11 @@
-import { getApp, getApps, initializeApp } from 'firebase/app'
-import { getAuth } from 'firebase/auth'
-import { getFirestore } from 'firebase/firestore'
+import { FirebaseApp, getApp, getApps, initializeApp } from 'firebase/app'
+import { Auth, connectAuthEmulator, getAuth } from 'firebase/auth'
+import {
+  connectFirestoreEmulator,
+  enableMultiTabIndexedDbPersistence,
+  Firestore,
+  getFirestore
+} from 'firebase/firestore'
 
 const firebaseConfig = {
   apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
@@ -11,13 +16,56 @@ const firebaseConfig = {
   appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID
 }
 
-let app
-if (!getApps().length) {
-  app = initializeApp(firebaseConfig) // Initializes app if no app exists
-} else {
-  app = getApp() // Uses existing app if app exists
+let app: FirebaseApp
+let auth: Auth
+let db: Firestore
+
+const clientSide = typeof window !== 'undefined'
+
+// Runs on the client side
+if (clientSide) {
+  app = getApps().length === 0 ? initializeApp(firebaseConfig) : getApp()
+  auth = getAuth(app)
+  db = getFirestore(app)
 }
 
-export const auth = getAuth(app)
-export const db = getFirestore(app)
-export default app
+// Runs on the client side
+if (clientSide) {
+  app = getApps().length === 0 ? initializeApp(firebaseConfig) : getApp()
+  auth = getAuth(app)
+  db = getFirestore(app)
+
+  // Use emulator if running in development and emualtor is running
+  if (location?.hostname === 'localhost') {
+    connectAuthEmulator(auth, 'http://localhost:9099')
+    connectFirestoreEmulator(db, 'localhost', 8080)
+    console.log('Connected to emulator')
+  }
+
+  // console.log(
+  //   `Emulator: ${process.env.FUNCTIONS_EMULATOR}, type: ${typeof process.env
+  //     .FUNCTIONS_EMULATOR}`
+  // )
+
+  // Enables offline support for firestore
+  enableMultiTabIndexedDbPersistence(db).catch((err) => {
+    if (err.code == 'failed-precondition') {
+      // Multiple tabs open, persistence can only be enabled
+      // in one tab at a a time.
+      // ...
+      console.log(
+        'The app is already open in another browser tab and multi-tab is not enabled'
+      )
+    } else if (err.code == 'unimplemented') {
+      // The current browser does not support all of the
+      // features required to enable persistence
+      // ...
+      console.log(
+        'The current browser does not support all of the features required to enable persistence'
+      )
+    }
+  })
+  // Subsequent queries will use persistence, if it was enabled successfully
+}
+
+export { auth, db }
