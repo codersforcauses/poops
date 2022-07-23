1a96f56f616d33e7fe765b3ca6625942aaeec465
James van de Ven <hello@jamesvandeven.com>
2022-07-23 10:15:44 +0800

    Moved incident form to visit instance

--- src/components/Visit/readvisitinstance.tsx
+++ src/components/Visit/readvisitinstance.tsx
@@ -1,6 +1,7 @@
+import { useState } from 'react'
 import { Timestamp } from 'firebase/firestore'
 
-import { ReportButton, VetConcernButton } from '@/components/Visit/buttons'
+import IncidentForm from '@/components/IncidentForm'
 import { formatDuration } from '@/components/Visit/visitinstance'
 import { VisitData } from '@/types/types'
 
@@ -9,6 +10,9 @@ const formatTime = (time: Timestamp) => {
 }
 
 const ReadOnlyVisitInstance = (props: VisitData) => {
+  const [isFormExpanded, setIsFormExpanded] = useState(false)
+  const [isVetVisit, setIsVetVisit] = useState(false)
+
   return (
     <>
       <div className='font-bold peer-checked:font-normal'>
@@ -24,12 +28,43 @@ const ReadOnlyVisitInstance = (props: VisitData) => {
         <p>Commute Distance: {props.commuteDist.toFixed(1)} km</p>
         <p>Commute Method: {props.commuteMethod}</p>
         <p>Notes: {props.notes}</p>
-        <div className='m-2 flex flex-row justify-around'>
-          {
-            // TODO: make stylings nice (gl James)
-          }
-          <VetConcernButton />
-          <ReportButton />
+        <div className='m-2 flex flex-col justify-around'>
+          <div className='flex space-x-1'>
+            <button
+              onClick={(e) => {
+                e.preventDefault()
+                setIsVetVisit(false)
+                if (isFormExpanded && !isVetVisit) {
+                  setIsFormExpanded(false)
+                } else {
+                  setIsFormExpanded(true)
+                }
+              }}
+              className='mx-auto mt-2 w-fit rounded-lg bg-primary py-1 px-4 text-lg text-white shadow-md focus:outline-primary active:bg-dark-red'
+            >
+              Create Incident
+            </button>
+            <button
+              onClick={(e) => {
+                e.preventDefault()
+                setIsVetVisit(true)
+                if (isFormExpanded && isVetVisit) {
+                  setIsFormExpanded(false)
+                  setIsVetVisit(false)
+                } else {
+                  setIsFormExpanded(true)
+                }
+              }}
+              className='mx-auto mt-2 w-fit rounded-lg bg-primary py-1 px-4 text-lg text-white shadow-md focus:outline-primary active:bg-dark-red'
+            >
+              Create Vet Incident
+            </button>
+          </div>
+          <IncidentForm
+            isExpanded={isFormExpanded}
+            isVetVisit={isVetVisit}
+            setIsExpanded={setIsFormExpanded}
+          />
         </div>
       </div>
     </>
--- src/pages/visit.tsx
+++ src/pages/visit.tsx
@@ -1,7 +1,6 @@
 import { useState } from 'react'
 
 import Header from '@/components/Header'
-import IncidentForm from '@/components/IncidentForm'
 import NavBar from '@/components/NavBar'
 import { withProtected } from '@/components/PrivateRoute'
 import { AddButton } from '@/components/Visit/buttons'
@@ -13,48 +12,10 @@ const Visit = () => {
   const [searchQuery, setSearchQuery] = useState('')
   const [isModalOpen, setIsModalOpen] = useState(false)
   const toggleModal = () => setIsModalOpen(!isModalOpen)
-  const [isFormExpanded, setIsFormExpanded] = useState(false)
-  const [isVetVisit, setIsVetVisit] = useState(false)
   return (
     <>
       <Header pageTitle='Visit' />
       <main>
-        <p>Visit Page</p>
-        <button
-          onClick={(e) => {
-            e.preventDefault()
-            setIsVetVisit(false)
-            if (isFormExpanded && !isVetVisit) {
-              setIsFormExpanded(false)
-            } else {
-              setIsFormExpanded(true)
-            }
-          }}
-          className='mx-auto mt-2 w-fit rounded-lg bg-primary py-1 px-4 text-lg text-white shadow-md focus:outline-primary active:bg-dark-red'
-        >
-          Create Incident
-        </button>
-        <button
-          onClick={(e) => {
-            e.preventDefault()
-            setIsVetVisit(true)
-            if (isFormExpanded && isVetVisit) {
-              setIsFormExpanded(false)
-              setIsVetVisit(false)
-            } else {
-              setIsFormExpanded(true)
-            }
-          }}
-          className='mx-auto mt-2 w-fit rounded-lg bg-primary py-1 px-4 text-lg text-white shadow-md focus:outline-primary active:bg-dark-red'
-        >
-          Create Vet Incident
-        </button>
-        <IncidentForm
-          isExpanded={isFormExpanded}
-          isVetVisit={isVetVisit}
-          setIsExpanded={setIsFormExpanded}
-        />
-        <p>Some other content</p>
         {!isModalOpen ? (
           <div className='flex w-screen flex-col p-4'>
             <div className='align-center flex flex-row justify-around'>
