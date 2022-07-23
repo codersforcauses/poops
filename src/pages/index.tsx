58f25bceb6ad543703308582fac65776947bfd4c
JadeynFeng <104485596+JadeynFeng@users.noreply.github.com>
2022-07-23 12:39:56 +0800

    dog image fix

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -10,7 +10,7 @@ const Home = () => {
   return (
     <>
       <Header pageTitle='Home' />
-      <main className='absolute h-[calc(100%-7rem)] overflow-y-scroll bg-[url(/images/dog-home.jpeg)] bg-contain bg-fixed bg-[left_50%_top_calc(100%-4rem)] bg-no-repeat'>
+      <main className='absolute h-[calc(100%-7rem)] overflow-y-scroll bg-[url(/images/dog-home.png)] bg-contain bg-fixed bg-[left_50%_top_calc(100%-4rem)] bg-no-repeat'>
         <div className='h-[calc(max-content +4rem)] m-auto flex w-screen flex-col'>
           <div className='flex flex-col px-4 '>
             <h1 className='py-3 text-center text-3xl'>
