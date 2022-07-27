173347bdc2c46793e4bc209de7578cb0c8be3420
Lison Lau <allisonlhm@gmail.com>
2022-07-27 22:50:19 +0800

    fix duration form css

--- src/components/Home/durationSelector.tsx
+++ src/components/Home/durationSelector.tsx
@@ -66,9 +66,9 @@ function DurationSelector(props: DurationProps) {
       </p>
 
       <div className='flex flex-row justify-center'>
-        <div>
+        <div className='mr-2'>
           <label htmlFor='hours'>Hours</label>
-          <div className='mr-1 flex flex-row justify-center'>
+          <div className='flex flex-row justify-center'>
             <input
               className='my-auto mr-2 flex h-9 rounded border border-[#6b7280] py-0.5 px-4 text-center focus:border-[#0066ff] focus:outline-none'
               id='hours'
@@ -78,7 +78,7 @@ function DurationSelector(props: DurationProps) {
               min={0}
               max={24}
             />
-            <div className='flex w-7 flex-col text-lg'>
+            <div className='flex w-5 flex-col text-sm'>
               <button
                 type='button'
                 className='my-0.5 rounded bg-zinc-300'
@@ -97,9 +97,9 @@ function DurationSelector(props: DurationProps) {
           </div>
         </div>
 
-        <div>
+        <div className='ml-2'>
           <label htmlFor='minutes'>Minutes</label>
-          <div className='ml-1 flex flex-row justify-center'>
+          <div className='flex flex-row justify-center'>
             <input
               className='my-auto mr-2 flex h-9 w-auto rounded border border-[#6b7280] py-0.5 px-4 text-center focus:border-[#0066ff] focus:outline-none'
               id='minutes'
@@ -109,7 +109,7 @@ function DurationSelector(props: DurationProps) {
               min={0}
               max={45}
             />
-            <div className='flex w-7 flex-col text-lg'>
+            <div className='flex w-5 flex-col text-sm'>
               <button
                 type='button'
                 className='my-0.5 rounded bg-zinc-300'
