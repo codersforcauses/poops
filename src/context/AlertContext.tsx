bcc82b0fe27031009ce5a59c24620dcc69d2d8bd
LisonLau <101380095+LisonLau@users.noreply.github.com>
2022-07-30 13:31:50 +0800

    Fix type errors

--- src/context/AlertContext.tsx
+++ src/context/AlertContext.tsx
@@ -1,4 +1,10 @@
-import React, { createContext, useCallback, useContext, useState } from 'react'
+import React, {
+  createContext,
+  ReactNode,
+  useCallback,
+  useContext,
+  useState
+} from 'react'
 
 import Alert from '@/components/UI/alert'
 
