ea8cca9a20d365c2631bbec7e4ca007f963f62b6
Nicholas Choong <nicholascwc1997@gmail.com>
2022-11-26 03:45:57 +0800

    temp disabled pwa

--- next.config.mjs
+++ next.config.mjs
@@ -1,14 +1,13 @@
 /** @type {import('next').NextConfig} */
 // const withPWA = require('next-pwa')
-import withPWA from 'next-pwa'
 
 // module.exports = withPWA({
-const nextConfig = withPWA({
-  pwa: {
-    dest: 'public',
-    register: true,
-    skipWaiting: true
-  },
+const nextConfig = {
+  // pwa: {
+  //   dest: 'public',
+  //   register: true,
+  //   skipWaiting: true
+  // },
 
   eslint: {
     dirs: ['src']
@@ -27,9 +26,34 @@ const nextConfig = withPWA({
   //     'res.cloudinary.com',
   //   ],
   // },
-})
+}
+// const nextConfig = withPWA({
+//   // pwa: {
+//   //   dest: 'public',
+//   //   register: true,
+//   //   skipWaiting: true
+//   // },
 
-// ! Temp fix for next-pwa throwing invalid next.config.js warning message
-delete nextConfig.pwa
+//   eslint: {
+//     dirs: ['src']
+//   },
+
+//   reactStrictMode: true,
+
+//   swcMinify: true,
+//   experimental: {
+//     optimizeCss: true
+//   }
+
+//   // Uncoment to add domain whitelist
+//   // images: {
+//   //   domains: [
+//   //     'res.cloudinary.com',
+//   //   ],
+//   // },
+// })
+
+// // ! Temp fix for next-pwa throwing invalid next.config.js warning message
+// delete nextConfig.pwa
 
 export default nextConfig
