c8add6dd24f4ee6283dd5d87e0b402a43077afa5
Dylan To <dylan.to@hotmail.com>
2022-11-30 23:46:29 +0800

    refac: use grid instead of table, feta: use reusable buttons

--- src/pages/visit/set.tsx
+++ src/pages/visit/set.tsx
@@ -5,7 +5,7 @@ import { XIcon } from '@heroicons/react/outline'
 import { Timestamp } from 'firebase/firestore'
 
 import { withProtected } from '@/components/PrivateRoute'
-import { ReportButton, VetConcernButton } from '@/components/Visit/buttons'
+import Button from '@/components/UI/button'
 import ClientSelector from '@/components/Visit/clientselector'
 import CommuteSelector from '@/components/Visit/commuteselector'
 import DurationSelector from '@/components/Visit/durationselector'
@@ -15,7 +15,7 @@ import { findContactIndex } from '@/components/Visit/utils'
 import { useFirestore } from '@/context/Firebase/Firestore/context'
 import { Duration, VisitData } from '@/types/types'
 
-const Visit = () => {
+const Set = () => {
   // BUG: when reloading this page whilst editing, all the fields are removed. userDoc is undefined?
   const { userDoc, updateVisit } = useFirestore()
   const router = useRouter()
@@ -69,6 +69,12 @@ const Visit = () => {
     // TODO: add alert?
   }
 
+  const handleDelete = () => {
+    userDoc.visits.splice(Number(id), 1)
+    updateVisit?.(userDoc) // can I guarantee visits will be updated by the time the client returns to /visit?
+    router.push('/visit')
+  }
+
   const isSubmitEnabled = () =>
     visitType &&
     clientName &&
@@ -78,172 +84,161 @@ const Visit = () => {
   walkDist >= 0 && commuteDist >= 0 && commuteMethod
 
   return (
-    <div className='z-50 p-4'>
+    <div className='space-4 z-50 flex flex-col p-4'>
+      {/* Exit Button */}
+      <div className='fixed right-5 top-4 z-[100] h-10 w-10 rounded-full bg-primary drop-shadow-default'>
+        <Link href='/visit'>
+          <button>
+            <XIcon className='h-full w-full text-white' />
+          </button>
+        </Link>
+      </div>
+
+      {/* Heading */}
       <>
-        <div className='fixed right-5 top-4 z-[100] h-10 w-10 rounded-full bg-primary p-1 drop-shadow-default'>
-          <Link href='/visit'>
-            <button>
-              <XIcon className='h-full w-full text-white' />
-            </button>
-          </Link>
-        </div>
+        <h1 className='p-2 text-2xl font-bold'>
+          {id !== null ? 'Edit' : 'Add'} Your Visit
+        </h1>
+        <div className='my-2 box-content border-t-2 border-primary' />
+      </>
 
-        <div className='border-b-2 border-primary py-3 pt-10'>
-          <h1 className='pl-2 text-2xl font-bold'>
-            {id !== null ? 'Edit' : 'Add'} Your Visit
-          </h1>
-        </div>
+      {/* Wrapper */}
+      <div className='container mx-auto flex flex-col gap-2 p-2'>
+        {/* Form */}
+        <form onSubmit={handleSubmit}>
+          {/* could rewrite to use awful react-select to make chevron icon consistent */}
+          <div className='grid grid-cols-2 gap-4'>
+            <FormField
+              id='visitTypeInput'
+              type='select'
+              placeholder='Select...'
+              value={visitType}
+              label='Visit Type:'
+              selectOptions={visitSelectOptions}
+              isRequired={true}
+              onChange={(event) => setVisitType(event.target.value)}
+            />
+            <ClientSelector
+              id='clientNameInput'
+              type='text'
+              placeholder='Client Name'
+              value={clientName}
+              label='Client Name:'
+              isRequired={true}
+              setClient={setClient}
+            />
 
-        <form className='pt-3' onSubmit={handleSubmit}>
-          <table className='container mx-auto table-fixed'>
-            <tbody>
-              <tr>
-                <td>
-                  {/* could rewrite to use awful react-select to make chevron icon consistent */}
-                  <FormField
-                    id='visitTypeInput'
-                    type='select'
-                    placeholder='Select...'
-                    value={visitType}
-                    label='Visit Type:'
-                    selectOptions={visitSelectOptions}
-                    isRequired={true}
-                    onChange={(event) => setVisitType(event.target.value)}
-                  />
-                </td>
-                <td>
-                  <ClientSelector
-                    id='clientNameInput'
-                    type='text'
-                    placeholder='Client Name'
-                    value={clientName}
-                    label='Client Name:'
-                    isRequired={true}
-                    setClient={setClient}
-                  />
-                </td>
-              </tr>
-              <tr>
-                <td>
-                  <FormField
-                    id='commuteDistInput'
-                    type='number'
-                    placeholder='Distance (km)'
-                    value={commuteDist.toString()}
-                    label='Commute Distance:'
-                    isRequired={true}
-                    onChange={(event) =>
-                      setCommuteDist(parseFloat(event.target.value))
-                    }
-                  />
-                </td>
-                <td>
-                  {/* react-select components have no native form validation but what we can do is disable the submit button if the input is empty */}
-                  <CommuteSelector
-                    id='commuteMethodInput'
-                    placeholder='Commute Method'
-                    value={commuteMethod}
-                    label='Commute Method:'
-                    setCommuteMethod={setCommuteMethod}
-                    isRequired={true}
-                  />
-                </td>
-              </tr>
-              <tr>
-                <td colSpan={2}>
-                  <FormField
-                    id='startTimeInput'
-                    type='dateTime-local'
-                    placeholder='Start Time'
-                    value={startTime}
-                    label='Start Time:'
-                    isRequired={true}
-                    onChange={(event) => {
-                      // event.target.value = event.target.value.substring(0, 16) // fixes invalid input on ios safari? can't test
-                      setStartTime(event.target.value)
-                    }}
-                  />
-                </td>
-              </tr>
-              <tr>
-                <td>
-                  <DurationSelector
-                    id='durationInput'
-                    label='Duration:'
-                    defaultValue={duration}
-                    onHourChange={(event) =>
-                      setDuration((duration) => ({
-                        ...duration,
-                        hours: Number(event.target.value)
-                      }))
-                    }
-                    onMinuteChange={(event) =>
-                      setDuration((duration) => ({
-                        ...duration,
-                        minutes:
-                          Math.round(Number(event.target.value) / 15) * 15
-                      }))
-                    }
-                  />
-                </td>
-                <td>
-                  <FormField
-                    id='walkDistInput'
-                    type='number'
-                    placeholder='Distance (km)'
-                    value={walkDist.toString()}
-                    label='Walk Distance:'
-                    isRequired={true}
-                    onChange={(event) =>
-                      setWalkDist(parseFloat(event.target.value))
-                    }
-                  />
-                </td>
-              </tr>
-            </tbody>
-          </table>
+            <FormField
+              id='commuteDistInput'
+              type='number'
+              placeholder='Distance (km)'
+              value={commuteDist.toString()}
+              label='Commute Distance:'
+              isRequired={true}
+              onChange={(event) =>
+                setCommuteDist(parseFloat(event.target.value))
+              }
+            />
+            {/* react-select components have no native form validation but what we can do is disable the submit button if the input is empty */}
+            <CommuteSelector
+              id='commuteMethodInput'
+              placeholder='Commute Method'
+              value={commuteMethod}
+              label='Commute Method:'
+              setCommuteMethod={setCommuteMethod}
+              isRequired={true}
+            />
 
-          <FormField
-            id='notesInput'
-            type='textarea'
-            placeholder='Add notes here'
-            value={notes}
-            label='Notes:'
-            isRequired={false}
-            onChange={(event) => setNotes(event.target.value)}
-          />
-          <div className='p- mx-auto my-2 flex flex-col '>
-            <button
-              className='text-bold rounded bg-primary px-12 py-4 text-white drop-shadow-default active:bg-dark-red disabled:bg-dark-gray'
+            <FormField
+              className='col-span-2'
+              id='startTimeInput'
+              type='dateTime-local'
+              placeholder='Start Time'
+              value={startTime}
+              label='Start Time:'
+              isRequired={true}
+              onChange={(event) => {
+                // event.target.value = event.target.value.substring(0, 16) // fixes invalid input on ios safari? can't test
+                setStartTime(event.target.value)
+              }}
+            />
+
+            <DurationSelector
+              id='durationInput'
+              label='Duration:'
+              defaultValue={duration}
+              onHourChange={(event) =>
+                setDuration((duration) => ({
+                  ...duration,
+                  hours: Number(event.target.value)
+                }))
+              }
+              onMinuteChange={(event) =>
+                setDuration((duration) => ({
+                  ...duration,
+                  minutes: Math.round(Number(event.target.value) / 15) * 15
+                }))
+              }
+            />
+
+            <FormField
+              id='walkDistInput'
+              type='number'
+              placeholder='Distance (km)'
+              value={walkDist.toString()}
+              label='Walk Distance:'
+              isRequired={true}
+              onChange={(event) => setWalkDist(parseFloat(event.target.value))}
+            />
+
+            <FormField
+              className='col-span-2'
+              id='notesInput'
+              type='textarea'
+              placeholder='Add notes here'
+              value={notes}
+              label='Notes:'
+              isRequired={false}
+              onChange={(event) => setNotes(event.target.value)}
+            />
+
+            <Button
+              className='col-span-2'
+              intent='primary'
               disabled={!isSubmitEnabled()}
+              fullwidth
+              size='large'
               type='submit'
             >
               Submit
-            </button>
-            <Link href='/visit'>
-              <button
-                type='button'
-                className='text-bold mt-2 rounded bg-primary p-1 px-12 py-4 text-white drop-shadow-default active:bg-dark-red'
-                onClick={() => {
-                  userDoc.visits.splice(Number(id), 1)
-                  updateVisit?.(userDoc)
-                }}
-                hidden={id === null} // button should be hidden if no id
-              >
-                Remove
-              </button>
-            </Link>
+            </Button>
+
+            <Button
+              className='col-span-2'
+              intent='secondary'
+              fullwidth
+              onClick={handleDelete}
+              hidden={id === null} // button should be hidden if no id
+            >
+              Remove This Visit
+            </Button>
+
+            <Button intent='primary' size='medium' fullwidth>
+              Report
+              <br />
+              Incident
+            </Button>
+
+            <Button intent='primary' size='medium' fullwidth>
+              Register Vet
+              <br />
+              Concern
+            </Button>
           </div>
         </form>
-        <div hidden={id === null}>
-          <VetConcernButton />
-        </div>
-        <div className='py-2' hidden={id === null}>
-          <ReportButton />
-        </div>
-      </>
+      </div>
     </div>
   )
 }
 
-export default withProtected(Visit)
+export default withProtected(Set)
