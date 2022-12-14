368d63c4a261903781a7a6f318bffac25dd1ccd2
Techno-UWA <23433982@student.uwa.edu.au>
2022-12-14 14:13:14 +0800

    Made MVP stats summary

--- src/components/Admin/Stats/summary.tsx
+++ src/components/Admin/Stats/summary.tsx
@@ -4,56 +4,57 @@ const dummyVisitData = '12'
 const dummyTimeData = '400'
 const dummyDistanceData = '6.5'
 
+const borderColor = ' border-gray-300 '
+const headerCellDecor = borderColor + 'border-4 p-6 font-extrabold text-2xl'
+
 function Summary() {
   return (
-    <div className='rounded-lg bg-zinc-200 py-4 px-5 text-center shadow-lg sm:py-4'>
+    <div className='rounded-lg py-4 px-5 text-center sm:py-4'>
       <h1 className='mb-2 text-xl text-dark-red'>
         <b>Admin Summary Dashboard</b>
       </h1>
       <hr className='h-0.5 border-dark-red bg-dark-red text-dark-red' />
       <div>
-        <table className='w-full'>
+        <table className={'w-full border-collapse border-spacing-2 border-8' + borderColor}>
+          <thead>
+            <tr>
+              <th className=''></th>
+              <th className={headerCellDecor}>Number of Clients</th>
+              <th className={headerCellDecor}>Number of Visits</th>
+              <th className={headerCellDecor}>Time Walked (mins)</th>
+              <th className={headerCellDecor}>Distance Walked Over the Period (kms)</th>
+            </tr>
+          </thead>
           <tbody>
             <tr>
-              <td>
-                <Statistics title='Total Number of Volunteer Visits' data={dummyVisitData} />
-              </td>
-              <td>
+              <th className={headerCellDecor}>Total</th>
                 <Statistics
-                  title='Total Time Walked in Minutes'
+                  title=''
+                  data={dummyVisitData}
+                />
+                <Statistics title='' data={dummyVisitData} />
+                <Statistics
+                  title=''
                   data={dummyTimeData + ' mins'}
                 />
-              </td>
-              <td>
                 <Statistics
-                  title='Total Distance Walked over the period'
+                  title=''
                   data={dummyDistanceData + ' km'}
                 />
-              </td>
             </tr>
 
             <tr>
-              <td>
-                <Statistics title='Average Number of Visits per Volunteer' data={dummyVisitData} />
-              </td>
-              <td>
-                <Statistics
-                  title='Average Time Walked in Minutes'
-                  data={dummyTimeData + ' mins'}
-                />
-              </td>
-              <td>
-                <Statistics
-                  title='Average Distance Walked over the period'
-                  data={dummyDistanceData + ' km'}
-                />
-              </td>
-            </tr>
-
-            <tr>
-              <td>
-                <Statistics title='Average Number of Clients' data={dummyVisitData} />
-              </td>
+              <th className={headerCellDecor}>Average (per volunteer)</th>
+              <Statistics title='' data={dummyVisitData} />
+              <Statistics title='' data={dummyVisitData} />
+              <Statistics
+                title=''
+                data={dummyTimeData + ' mins'}
+              />
+              <Statistics
+                title=''
+                data={dummyDistanceData + ' km'}
+              />
             </tr>
           </tbody>
         </table>
