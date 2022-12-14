bf89d86685b8dfe9cd52849cc8e501bc71eaa01d
Techno-UWA <23433982@student.uwa.edu.au>
2022-12-14 14:11:29 +0800

    Put <td> inside stats + styled stats table cells

--- src/components/Admin/Stats/statistics.tsx
+++ src/components/Admin/Stats/statistics.tsx
@@ -3,12 +3,15 @@ interface SummaryStatistics {
   data: string
 }
 
+const borderColor = ' border-gray-300 '
+const cellDecor = borderColor + 'border-4 p-10'
+
 function Statistics(props: SummaryStatistics) {
   return (
-    <div>
-      <p className='mt-2 text-lg'>{props.title}</p>
-      <p className='text-3xl text-dark-red'>{props.data}</p>
-    </div>
+      <td className={cellDecor}>
+        <p className='mt-2 text-lg'>{props.title}</p>
+        <p className='text-3xl text-dark-red font-extrabold'>{props.data}</p>
+      </td>
   )
 }
 
