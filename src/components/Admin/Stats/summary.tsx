5d36492811745a9a165b4255a93d1330c2993cca
Techno-UWA <23433982@student.uwa.edu.au>
2022-12-16 09:28:36 +0800

    Abstracted Summary a bit

--- src/components/Admin/Stats/summary.tsx
+++ src/components/Admin/Stats/summary.tsx
@@ -7,59 +7,51 @@ const dummyDistanceData = '6.5'
 const borderColor = ' border-gray-300 '
 const headerCellDecor = borderColor + 'border-4 p-6 font-extrabold text-2xl'
 
+const dummyUsername = 'Admin'
+
 function Summary() {
   return (
-    <div className='rounded-lg py-4 px-5 text-center sm:py-4'>
-      <h1 className='mb-2 text-xl text-dark-red'>
-        <b>Admin Summary Dashboard</b>
+    <>
+      <h1 className='py-3 text-center text-3xl'>
+        Welcome, {dummyUsername}!
       </h1>
-      <hr className='h-0.5 border-dark-red bg-dark-red text-dark-red' />
-      <div>
-        <table className={'w-full border-collapse border-spacing-2 border-8' + borderColor}>
-          <thead>
-            <tr>
-              <th className=''></th>
-              <th className={headerCellDecor}>Number of Clients</th>
-              <th className={headerCellDecor}>Number of Visits</th>
-              <th className={headerCellDecor}>Time Walked (mins)</th>
-              <th className={headerCellDecor}>Distance Walked Over the Period (kms)</th>
-            </tr>
-          </thead>
-          <tbody>
-            <tr>
-              <th className={headerCellDecor}>Total</th>
-                <Statistics
-                  title=''
-                  data={dummyVisitData}
-                />
-                <Statistics title='' data={dummyVisitData} />
-                <Statistics
-                  title=''
-                  data={dummyTimeData + ' mins'}
-                />
-                <Statistics
-                  title=''
-                  data={dummyDistanceData + ' km'}
-                />
-            </tr>
+      <div className='rounded-lg py-4 px-5 text-center sm:py-4'>
+        <h1 className='mb-2 text-xl text-dark-red'>
+          <b>Admin Summary Dashboard</b>
+        </h1>
+        <hr className='h-0.5 border-dark-red bg-dark-red text-dark-red' />
+        <div>
+          <table className={'w-full border-collapse border-spacing-2 border-8' + borderColor}>
+            <thead>
+              <tr>
+                <th className=''></th>
+                <th className={headerCellDecor}>Number of Clients</th>
+                <th className={headerCellDecor}>Number of Visits</th>
+                <th className={headerCellDecor}>Time Walked (mins)</th>
+                <th className={headerCellDecor}>Distance Walked Over the Period (kms)</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <th className={headerCellDecor}>Total</th>
+                <Statistics data={dummyVisitData} />
+                <Statistics data={dummyVisitData} />
+                <Statistics data={dummyTimeData + ' mins'} />
+                <Statistics data={dummyDistanceData + ' km'} />
+              </tr>
 
-            <tr>
-              <th className={headerCellDecor}>Average (per volunteer)</th>
-              <Statistics title='' data={dummyVisitData} />
-              <Statistics title='' data={dummyVisitData} />
-              <Statistics
-                title=''
-                data={dummyTimeData + ' mins'}
-              />
-              <Statistics
-                title=''
-                data={dummyDistanceData + ' km'}
-              />
-            </tr>
-          </tbody>
-        </table>
+              <tr>
+                <th className={headerCellDecor}>Average (per volunteer)</th>
+                <Statistics data={dummyVisitData} />
+                <Statistics data={dummyVisitData} />
+                <Statistics data={dummyTimeData + ' mins'} />
+                <Statistics data={dummyDistanceData + ' km'} />
+              </tr>
+            </tbody>
+          </table>
+        </div>
       </div>
-    </div>
+    </>
   )
 }
 
