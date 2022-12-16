52367e75267720889a31170920cb1433dbf89056
Techno-UWA <23433982@student.uwa.edu.au>
2022-12-16 09:53:19 +0800

    Moved welcome under admin optional title prop

--- src/components/Admin/AdminPage/adminPage.tsx
+++ src/components/Admin/AdminPage/adminPage.tsx
@@ -1,12 +1,15 @@
+import { ReactNode } from "react"
+
 import Header from "@/components/Header"
 import NavBar from "@/components/NavBar"
 import TopNav from "@/components/TopNav"
 
 interface ParentCompProps {
-  content?: React.ReactNode
+  title?: string
+  content?: ReactNode
 }
 
-const AdminPage= ({ content }: ParentCompProps) => {
+const AdminPage= ({ title, content }: ParentCompProps) => {
   return (
     <>
       <Header pageTitle='Home' />
@@ -14,6 +17,9 @@ const AdminPage= ({ content }: ParentCompProps) => {
       <main className='absolute h-[calc(100%-7rem)] overflow-y-scroll bg-contain bg-fixed bg-[left_50%_top_calc(100%-4rem)] bg-no-repeat'>
         <div className='h-[calc(max-content +4rem)] m-auto flex w-screen flex-col'>
           <div className='flex flex-col px-4 '>
+            <h1 className='py-3 text-center text-3xl'>
+              { title }
+            </h1>
             { content }
           </div>
         </div>
