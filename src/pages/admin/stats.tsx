4f3e48679151dc09652b1fa5407f2007b5e67bf5
Techno-UWA <23433982@student.uwa.edu.au>
2022-12-16 09:23:02 +0800

    Abstracted stats by making AdminPage comp

--- src/pages/admin/stats.tsx
+++ src/pages/admin/stats.tsx
@@ -1,27 +1,9 @@
+import AdminPage from '@/components/Admin/AdminPage/adminPage'
 import Summary from '@/components/Admin/Stats/summary'
-import Header from '@/components/Header'
-import NavBar from '@/components/NavBar'
-import TopNav from '@/components/TopNav'
-
-const dummyUsername = 'Admin'
 
 const Stats = () => {
   return (
-    <>
-      <Header pageTitle='Home' />
-      <TopNav />
-      <main className='absolute h-[calc(100%-7rem)] overflow-y-scroll bg-contain bg-fixed bg-[left_50%_top_calc(100%-4rem)] bg-no-repeat'>
-        <div className='h-[calc(max-content +4rem)] m-auto flex w-screen flex-col'>
-          <div className='flex flex-col px-4 '>
-            <h1 className='py-3 text-center text-3xl'>
-              Welcome, {dummyUsername}!
-            </h1>
-            <Summary />
-          </div>
-        </div>
-      </main>
-      <NavBar />
-    </>
+    <AdminPage content={<Summary />} />
   )
 }
 
