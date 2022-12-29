06b7636b698a1cde9cb90bfcaf14ac5b5a9149eb
Nicholas Choong <nicholascwc1997@gmail.com>
2022-12-30 01:01:42 +0800

    added mod me button in home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,14 +5,23 @@ import Summary from '@/components/Home/summary'
 import NavBar from '@/components/NavBar'
 import TopNav from '@/components/TopNav'
 import Button from '@/components/UI/button'
+import { useAuth } from '@/context/Firebase/Auth/context'
 import useUser from '@/hooks/user'
+import mod from '@/lib/temp/firebase/functions/setRole'
 
 const Home = () => {
-  const { isSuccess, data: currentUser } = useUser()
+  const { currentUser, getUserToken } = useAuth()
+  const { isSuccess } = useUser()
 
   const welcomeMessage = isSuccess
-    ? `Welcome, ${currentUser?.name}!`
+    ? `Welcome, ${currentUser?.displayName}!`
     : 'Welcome!'
+
+  const onMod = (adminAccess: boolean) => {
+    if (currentUser) {
+      mod(adminAccess, currentUser, getUserToken)
+    }
+  }
 
   return (
     <>
@@ -24,10 +33,19 @@ const Home = () => {
             <h1 className='py-3 text-center text-3xl'>{welcomeMessage}</h1>
             <Summary />
             <br />
-            <div className='flex justify-center'>
+            <div className='flex flex-col justify-center'>
               <Link href='visit/set'>
                 <Button size='large'>START VISIT</Button>
               </Link>
+              <br />
+              <Button
+                size='medium'
+                intent='secondary'
+                type='button'
+                onClick={() => onMod(true)}
+              >
+                Mod me!
+              </Button>
             </div>
             <br />
             <br />
