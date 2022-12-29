56c0dbf550fc1b8c44f4b8ab09f96eb4e5da194e
Nicholas Choong <nicholascwc1997@gmail.com>
2022-12-30 01:01:26 +0800

    standardised setRole func

--- src/lib/temp/firebase/functions/setRole.ts
+++ src/lib/temp/firebase/functions/setRole.ts
@@ -1,9 +1,11 @@
+import { User } from '@firebase/auth'
+
 interface ISetRoles {
   email: string
   roles: Record<string, boolean>
 }
 
-const setRoles = async (data: ISetRoles) => {
+const setRole = async (data: ISetRoles) => {
   const response = await fetch('/api/setRole', {
     method: 'POST',
     body: JSON.stringify(data)
@@ -11,4 +13,26 @@ const setRoles = async (data: ISetRoles) => {
   return response
 }
 
-export default setRoles
+const mod = async (
+  adminAccess: boolean,
+  currentUser: User,
+  getUserToken: () => Promise<void>,
+  getUserDoc?: () => Promise<void>
+) => {
+  try {
+    if (currentUser?.email) {
+      const reponse = await setRole({
+        email: currentUser.email,
+        roles: { admin: adminAccess }
+      })
+      const data = await reponse.json()
+      console.log('Next API Response', data)
+      getUserToken()
+      getUserDoc?.()
+    }
+  } catch (error) {
+    console.log('Firebase Functions Error: ', error)
+  }
+}
+
+export default mod
