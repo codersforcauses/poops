19a2b0a658efd52c21c3a2a807ab73d6c658fc41
Nicholas Choong <nicholascwc1997@gmail.com>
2022-12-30 01:43:35 +0800

    debug on vercel

--- src/lib/temp/firebase/admin/init.ts
+++ src/lib/temp/firebase/admin/init.ts
@@ -2,13 +2,17 @@ import { App, cert, getApps, initializeApp } from 'firebase-admin/app'
 import { Auth, getAuth } from 'firebase-admin/auth'
 import { Firestore, getFirestore } from 'firebase-admin/firestore'
 
+const firebaseAdminConfig = {
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+  privateKey: process.env.FIREBASE_PRIVATE_KEY
+}
+
+console.log(firebaseAdminConfig)
+
 const app: App = !getApps().length
   ? initializeApp({
-      credential: cert({
-        projectId: process.env.FIREBASE_PROJECT_ID,
-        clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
-        privateKey: process.env.FIREBASE_PRIVATE_KEY
-      }),
+      credential: cert(firebaseAdminConfig),
       databaseURL: process.env.FIREBASE_DATABASE_URL
     })
   : getApps()[0]
