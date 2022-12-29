1eaa1f822dfc56cabec8ef900fb0d1dbe50a6308
Nicholas Choong <nicholascwc1997@gmail.com>
2022-12-30 01:00:36 +0800

    moved API keys to .env

--- src/lib/temp/firebase/admin/init.ts
+++ src/lib/temp/firebase/admin/init.ts
@@ -1,21 +1,15 @@
-import {
-  App,
-  cert,
-  getApps,
-  initializeApp,
-  ServiceAccount
-} from 'firebase-admin/app'
+import { App, cert, getApps, initializeApp } from 'firebase-admin/app'
 import { Auth, getAuth } from 'firebase-admin/auth'
 import { Firestore, getFirestore } from 'firebase-admin/firestore'
 
-import firebaseAdminConfig from './firebaseAdminConfig'
-
-const adminConfig = firebaseAdminConfig as ServiceAccount
-
 const app: App = !getApps().length
   ? initializeApp({
-      credential: cert(adminConfig),
-      databaseURL: 'https://poops-9dbf6.firebaseio.com'
+      credential: cert({
+        projectId: process.env.FIREBASE_PROJECT_ID,
+        clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+        privateKey: process.env.FIREBASE_PRIVATE_KEY
+      }),
+      databaseURL: process.env.FIREBASE_DATABASE_URL
     })
   : getApps()[0]
 
