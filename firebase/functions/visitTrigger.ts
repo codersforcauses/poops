dcc141b5730f300f9e7858ffa7ec05b0804476d2
justinliangg <justin4426@outlook.com>
2022-12-30 09:10:40 +0800

    Refactor trigger and added more comments

--- firebase/functions/visitTrigger.ts
+++ firebase/functions/visitTrigger.ts
@@ -2,11 +2,14 @@ import { firestore, app } from './main'
 import { getFirestore } from 'firebase-admin/firestore'
 const db = getFirestore(app)
 
+/**
+ * Trigger for any changes to the visit subcollection
+ * Automatically updates the user stats when a visit is added,
+ * deleted or updated.
+ */
 export const updateVisitTrigger = firestore
   .document('users/{userId}/visits/{visitId}')
   .onWrite(async (change, context) => {
-    const oldVisit = change.before.exists ? change.before.data() : null
-    const newVisit = change.after.exists ? change.after.data() : null
     const userId = context.params.userId
     const userRef = db.collection('users').doc(userId)
 
@@ -14,7 +17,10 @@ export const updateVisitTrigger = firestore
     const userDoc = await userRef.get()
     const oldStats = userDoc.data()?.stats
 
-    // prepping difference in old and new visit
+    // The values of oldVisit and newVisit can be used to determine what
+    // operation was done on the visit subcollection
+    const oldVisit = change.before.exists ? change.before.data() : null
+    const newVisit = change.after.exists ? change.after.data() : null
     let hoursDiff = 0
     let commuteDistDiff = 0
     let walkDistDiff = 0
@@ -42,7 +48,7 @@ export const updateVisitTrigger = firestore
       walkDistDiff += newVisit.walkDist
     }
 
-    // updating user stats with new visit data
+    // updating user stats with any changes made to the visit subcollection
     return userRef.update({
       stats: {
         numVisits: oldStats.numVisits + visitDiff,
