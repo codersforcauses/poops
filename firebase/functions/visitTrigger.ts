6bc0ff03cec31b83c55f6392cfbf3de639c7c3f4
justinliangg <justin4426@outlook.com>
2022-12-31 19:20:16 +0800

    Added premature exit of trigger when relevant visit details is not changed and refactored

--- firebase/functions/visitTrigger.ts
+++ firebase/functions/visitTrigger.ts
@@ -1,5 +1,6 @@
 import { firestore, app } from './main'
-import { getFirestore } from 'firebase-admin/firestore'
+import { getFirestore, DocumentData } from 'firebase-admin/firestore'
+import { UserStat } from '../../src/types/types'
 const db = getFirestore(app)
 
 /**
@@ -11,50 +12,81 @@ export const updateVisitTrigger = firestore
   .document('users/{userId}/visits/{visitId}')
   .onWrite(async (change, context) => {
     const userId = context.params.userId
-    const userRef = db.collection('users').doc(userId)
-
-    // getting old user stats
-    const userDoc = await userRef.get()
-    const oldStats = userDoc.data()?.stats
-
-    // The values of oldVisit and newVisit can be used to determine what
-    // operation was done on the visit subcollection
     const oldVisit = change.before.exists ? change.before.data() : null
     const newVisit = change.after.exists ? change.after.data() : null
-    let hoursDiff = 0
-    let commuteDistDiff = 0
-    let walkDistDiff = 0
-    let visitDiff = 0
 
-    if (newVisit && oldVisit) {
-      // visit was updated
-      hoursDiff =
-        newVisit.duration.hours +
-        newVisit.duration.minutes / 60 -
-        (oldVisit.duration.hours + oldVisit.duration.minutes / 60)
-      commuteDistDiff = newVisit.commuteDist - oldVisit.commuteDist
-      walkDistDiff = newVisit.walkDist - oldVisit.walkDist
-    } else if (oldVisit) {
-      // visit was deleted
-      visitDiff -= 1
-      hoursDiff -= oldVisit.duration.hours + oldVisit.duration.minutes / 60
-      commuteDistDiff -= oldVisit.commuteDist
-      walkDistDiff -= oldVisit.walkDist
-    } else if (newVisit) {
-      // visit was added
-      visitDiff += 1
-      hoursDiff += newVisit.duration.hours + newVisit.duration.minutes / 60
-      commuteDistDiff += newVisit.commuteDist
-      walkDistDiff += newVisit.walkDist
+    // relevant visit fields have not changed so can just exit.
+    if (!visitChanged(oldVisit, newVisit)) {
+      return null
     }
 
-    // updating user stats with any changes made to the visit subcollection
+    // require oldStats to append to newStats
+    const userRef = db.collection('users').doc(userId)
+    const userDoc = await userRef.get()
+    const oldStats: UserStat = userDoc.data()?.stats
+
+    // updating the doc in firestore
+    const newStats = getUpdatedUserStats(oldStats, oldVisit, newVisit)
     return userRef.update({
-      stats: {
-        numVisits: oldStats.numVisits + visitDiff,
-        numHours: oldStats.numHours + hoursDiff,
-        commutedDist: oldStats.commutedDist + commuteDistDiff,
-        walkedDist: oldStats.walkedDist + walkDistDiff
-      }
+      stats: newStats
     })
   })
+
+const visitChanged = (
+  oldVisit?: DocumentData | null,
+  newVisit?: DocumentData | null
+) => {
+  // visit was added or deleted, so has changed
+  if (oldVisit == null || newVisit == null) return true
+
+  // visit was updated
+  // so checking if relevant visit fields have changed.
+  if (oldVisit.commuteDist !== newVisit.commuteDist) return true
+  if (oldVisit.walkDist !== newVisit.walkDist) return true
+  if (oldVisit.duration.hours !== newVisit.duration.hours) return true
+  if (oldVisit.duration.minutes !== newVisit.duration.minutes) return true
+
+  return false
+}
+
+const getUpdatedUserStats = (
+  oldStats: UserStat,
+  oldVisit?: DocumentData | null,
+  newVisit?: DocumentData | null
+) => {
+  // appending the oldStats to the newStats first.
+  const newStats: UserStat = {
+    numHours: oldStats.numHours,
+    numVisits: oldStats.numVisits,
+    commutedDist: oldStats.commutedDist,
+    walkedDist: oldStats.walkedDist
+  }
+
+  // The values of oldVisit and newVisit can be used to determine what
+  // operation was done on the visit subcollection
+  if (newVisit && oldVisit) {
+    // visit was updated
+    newStats.numHours +=
+      newVisit.duration.hours +
+      newVisit.duration.minutes / 60 -
+      (oldVisit.duration.hours + oldVisit.duration.minutes / 60)
+    newStats.commutedDist += newVisit.commuteDist - oldVisit.commuteDist
+    newStats.walkedDist += newVisit.walkDist - oldVisit.walkDist
+  } else if (oldVisit) {
+    // visit was deleted
+    newStats.numVisits -= 1
+    newStats.numHours -=
+      oldVisit.duration.hours + oldVisit.duration.minutes / 60
+    newStats.commutedDist -= oldVisit.commuteDist
+    newStats.walkedDist -= oldVisit.walkDist
+  } else if (newVisit) {
+    // visit was added
+    newStats.numVisits += 1
+    newStats.numHours +=
+      newVisit.duration.hours + newVisit.duration.minutes / 60
+    newStats.commutedDist += newVisit.commuteDist
+    newStats.walkedDist += newVisit.walkDist
+  }
+
+  return newStats
+}
