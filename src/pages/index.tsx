aa4299b4c36714c62d286cb16cb5d24280d29f1c
jordantgiang <jordantgiang@gmail.com>
2023-01-14 14:46:57 +0800

    Home page layout

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,15 +1,15 @@
+import {ReactElement} from 'react'
 import Link from 'next/link'
 
-import Header from '@/components/Header'
 import Summary from '@/components/Home/summary'
-import NavBar from '@/components/NavBar'
-import { withProtected } from '@/components/PrivateRoute'
-import TopNav from '@/components/TopNav'
+import Layout from '@/components/Layout'
 import Button from '@/components/UI/button'
 import { useAuth } from '@/context/Firebase/Auth/context'
 import mod from '@/lib/temp/firebase/functions/setRole'
 
-const Home = () => {
+import { NextPageWithLayout } from './_app'
+
+const Home: NextPageWithLayout = () => {
   const { currentUser, refreshUserToken } = useAuth()
 
   const welcomeMessage = currentUser
@@ -23,39 +23,41 @@ const Home = () => {
   }
 
   return (
-    <>
-      <Header pageTitle='Home' />
-      <TopNav />
-      <main className='absolute h-[calc(100%-7rem)] overflow-y-scroll bg-[url(/images/dog-home.png)] bg-contain bg-fixed bg-[left_50%_top_calc(100%-4rem)] bg-no-repeat'>
-        <div className='h-[calc(max-content +4rem)] m-auto flex w-screen flex-col'>
-          <div className='flex flex-col px-4 '>
-            <h1 className='py-3 text-center text-3xl'>{welcomeMessage}</h1>
-            <Summary />
+    <div className='h-full bg-[url(/images/dog-home.png)] bg-contain bg-fixed bg-[left_50%_top_calc(100%-4rem)] bg-no-repeat'>
+      <div className='h-[calc(max-content +4rem)] m-auto flex w-screen flex-col'>
+        <div className='flex flex-col px-4 '>
+          <h1 className='py-3 text-center text-3xl'>{welcomeMessage}</h1>
+          <Summary />
+          <br />
+          <div className='flex flex-col justify-center'>
+            <Link href='visit/set'>
+              <a className='flex justify-center'>
+                <Button size='large'>START VISIT</Button>
+              </a>
+            </Link>
             <br />
-            <div className='flex flex-col justify-center'>
-              <Link href='visit/set'>
-                <a className='flex justify-center'>
-                  <Button size='large'>START VISIT</Button>
-                </a>
-              </Link>
-              <br />
-              <Button
-                size='medium'
-                intent='secondary'
-                type='button'
-                onClick={() => onMod(true)}
-              >
-                Mod me!
-              </Button>
-            </div>
-            <br />
-            <br />
+            <Button
+              size='medium'
+              intent='secondary'
+              type='button'
+              onClick={() => onMod(true)}
+            >
+              Mod me!
+            </Button>
           </div>
+          <br />
+          <br />
         </div>
-      </main>
-      <NavBar />
-    </>
+      </div>
+    </div>
   )
 }
 
-export default withProtected(Home)
+Home.getLayout = function getLayout(page: ReactElement) {
+  return (
+    <Layout title="Home">{page}</Layout>
+  )
+}
+
+// export default withProtected(Home) // withProtected breaks the layout
+export default Home
