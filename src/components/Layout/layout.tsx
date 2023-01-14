1c03b4e4f794af091e38fd4696b8c337ed706703
HUIXIN-TW <huixin.yang.tw@gmail.com>
2023-01-14 14:01:54 +0800

    create a layout

---
+++ src/components/Layout/layout.tsx
@@ -0,0 +1,22 @@
+import { ReactNode } from 'react'
+import TopNav from '@/components/TopNav'
+import NavBar from '@/components/NavBar'
+import Header from '@/components/Header'
+
+export interface LayoutInterface {
+    title: string, 
+    children?: ReactNode
+}
+
+const Layout = ({ children, title }: LayoutInterface) => {
+    return (
+        <>
+            <Header pageTitle={title} />
+            <TopNav />
+            <main>{children}</main>
+            <NavBar />
+        </>
+    )
+}
+
+export default Layout
