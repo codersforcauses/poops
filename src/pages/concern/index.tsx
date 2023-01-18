08c4b55fc0706a4e9f523e34933739f6ce077253
Kyle Leung <lkkc0300@gmail.com>
2023-01-18 16:49:55 +0800

    dummy data added

--- src/pages/concern/index.tsx
+++ src/pages/concern/index.tsx
@@ -3,6 +3,18 @@ import { XMarkIcon } from '@heroicons/react/24/outline'
 
 import { withProtected } from '@/components/PrivateRoute'
 
+/* Dummy Data */
+const uid = '123456'
+const uname = 'Quokka'
+const uemail = 'Quokka@rottnest.com'
+const uphone = 999888777
+const cname = 'Koala'
+const pname = 'Pet'
+const vtime = '[timestamp to be added]'
+const vid = '123456'
+const detail = 'Pet was hurt!'
+const ctime = '[timestamp to be added]'
+
 const Concern = () => {
   return (
     <div className='space-4 z-50 flex h-full flex-col p-4'>
@@ -21,16 +33,16 @@ const Concern = () => {
 
       {/* Content */}
       <div className='grid'>
-        <h1 class='p-2'>user_uid: string</h1>
-        <h1 class='p-2'>user_name: string</h1>
-        <h1 class='p-2'>user_email: string</h1>
-        <h1 class='p-2'>user_phone: integer</h1>
-        <h1 class='p-2'>client_name: string</h1>
-        <h1 class='p-2'>pet_name: string</h1>
-        <h1 class='p-2'>visit_time: timestamp</h1>
-        <h1 class='p-2'>visit_id: string</h1>
-        <h1 class='p-2'>detail: string</h1>
-        <h1 class='p-2'>created_at: timestamp</h1>
+        <h1 class='p-2'>user_uid: {uid}</h1>
+        <h1 class='p-2'>user_name: {uname}</h1>
+        <h1 class='p-2'>user_email: {uemail}</h1>
+        <h1 class='p-2'>user_phone: {uphone}</h1>
+        <h1 class='p-2'>client_name: {cname}</h1>
+        <h1 class='p-2'>pet_name: {pname}</h1>
+        <h1 class='p-2'>visit_time: {vtime}</h1>
+        <h1 class='p-2'>visit_id: {vid}</h1>
+        <h1 class='p-2'>detail: {detail}</h1>
+        <h1 class='p-2'>created_at: {ctime}</h1>
       </div>
     </div>
   )
