016ad998405338f92ec7d75bd5567257ec4644fd
Amy Neve <ladyames20203@gmail.com>
2023-01-21 15:33:28 +0800

    first chunk

--- src/components/Home/summary.tsx
+++ src/components/Home/summary.tsx
@@ -1,9 +1,21 @@
 import Statistics from '@/components/Home/statistics'
+import Spinner from '@/components/UI/loadingSpinner'
 import useUser from '@/hooks/user'
 
 function Summary() {
-  const { isSuccess, data: currentUser } = useUser()
-
+  const { isLoading, isSuccess, data: currentUser } = useUser()
+  if (isLoading)
+    return (
+      <div className='rounded-lg bg-zinc-100 py-4 px-5 text-center shadow-lg sm:py-4'>
+        <h1 className='mb-2 text-xl text-primary-dark'>
+          <b>Summary Dashboard</b>
+        </h1>
+        <hr className='h-0.5 border-primary-dark bg-primary-dark text-primary-dark' />
+        <div className='flex h-20 items-center justify-center'>
+          <Spinner style='h-10 w-10 fill-primary-dark text-gray-200' />
+        </div>
+      </div>
+    )
   if (!isSuccess || currentUser == undefined || currentUser.stats == undefined)
     return null
 
