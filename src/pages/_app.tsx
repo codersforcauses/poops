82650552f523f52e7ac3fe8a490a83f5a63ad743
Amy Neve <ladyames20203@gmail.com>
2023-01-21 15:36:52 +0800

    second chunk

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,4 +1,7 @@
+import { ReactElement, ReactNode } from 'react'
+import { NextPage } from 'next'
 import { AppProps } from 'next/app'
+import Head from 'next/head'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 
@@ -9,13 +12,28 @@ import '@/styles/main.css'
 
 const queryClient = new QueryClient()
 
-const POOPS = ({ Component, pageProps }: AppProps) => {
+export type NextPageWithLayout = NextPage & {
+  getLayout?: (page: ReactElement) => ReactNode
+}
+
+type AppPropsWithLayout = AppProps & {
+  Component: NextPageWithLayout
+}
+
+const POOPS = ({ Component, pageProps }: AppPropsWithLayout) => {
+  const getLayout = Component.getLayout ?? ((page) => page)
   return (
     <>
+      <Head>
+        <meta
+          name='viewport'
+          content='width=device-width, initial-scale=1 viewport-fit=cover'
+        />
+      </Head>
       <AuthContextProvider>
         <AlertContextProvider>
           <QueryClientProvider client={queryClient}>
-            <Component {...pageProps} />
+            {getLayout(<Component {...pageProps} />)}
             <ReactQueryDevtools />
           </QueryClientProvider>{' '}
         </AlertContextProvider>
