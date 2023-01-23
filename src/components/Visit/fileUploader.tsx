62ec5befadf4583983d70f0176bcfce5f9a513f4
jordantgiang <jordantgiang@gmail.com>
2023-01-23 14:50:29 +0800

    Create fileUploader component

---
+++ src/components/Visit/fileUploader.tsx
@@ -0,0 +1,47 @@
+import { useRef } from 'react'
+
+import Button from '@/components/UI/button'
+
+export interface fileUploaderInterface {
+  label: string
+  handleFile(f: File): void
+}
+const FileUploader = ({
+  label,
+  handleFile
+}: fileUploaderInterface) => {
+
+  const hiddenFileInput = useRef<HTMLInputElement>(null)
+
+  const handleClick = () => {
+    if (hiddenFileInput.current) {
+      hiddenFileInput.current.click()
+    }
+  }
+
+  const handleChange = (e: React.FormEvent<HTMLInputElement>) => {
+    if (e.currentTarget.files){ 
+      const fileUploaded = e.currentTarget.files[0]
+      handleFile(fileUploaded)
+    }
+  }
+
+  return (
+    <>
+      <Button 
+        size="small"
+        onClick={handleClick}
+      >
+        {label}
+      </Button>
+      <input 
+        type="file" 
+        ref={hiddenFileInput}
+        onChange={handleChange}
+        className="hidden"
+      />
+    </>
+  )
+}
+
+export default FileUploader
