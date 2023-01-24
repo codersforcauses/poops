366068f2ec4e4ef59a81417371e7c62043bfbbf1
Dylan To <dylan.to@hotmail.com>
2023-01-24 21:26:55 +0800

    feat: only allow poops.org domain

--- firebase/functions/index.ts
+++ firebase/functions/index.ts
@@ -1,4 +1,5 @@
 export * from './callable/pubsubWriter'
 export * from './trigger/billingKillswitch'
 export * from './trigger/visitTrigger'
+export * from './trigger/addAdmin'
 export * from './callable/createAdmin'
