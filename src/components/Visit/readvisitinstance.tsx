b5e523a7d76f69256f86bd0ecf32565b8ffc3be5
Dylan To <dylan.to@hotmail.com>
2023-01-26 03:00:34 +0800

    fix: flyout animation

--- src/components/Visit/readvisitinstance.tsx
+++ src/components/Visit/readvisitinstance.tsx
@@ -33,65 +33,65 @@ const VisitInfo = ({
   if (docId === undefined) return null
 
   return (
-    <>
-      <div
-        className={`mt-2 justify-between text-sm transition-all duration-300 ${
-          isOpen ? '' : 'max-h-0 overflow-hidden'
-        }`}
-      >
-        <div className='max-h-screen space-y-1'>
-          <div className='space-x-1'>
-            <span className='font-semibold'>Visit Type:</span>
-            <span>{type}</span>
-          </div>
-          <div className='space-x-1'>
-            <span className='font-semibold'>Pet(s):</span>
-            <span>{petNames}</span>
-          </div>
-          <div className='space-x-1'>
-            <span className='font-semibold'>Duration:</span>
-            <span>{formatDuration(duration)}</span>
-          </div>
-          <div className='space-x-1'>
-            <span className='font-semibold'>Walk Distance:</span>
-            <span>{walkDist.toFixed(3)} km</span>
-          </div>
-          <div className='space-x-1'>
-            <span className='font-semibold'>Commute Distance:</span>
-            <span>{commuteDist.toFixed(1)} km</span>
-          </div>
-          <div className='space-x-1'>
-            <span className='font-semibold'>Commute Method:</span>
-            <span>{commuteMethod}</span>
-          </div>
-          <div>
-            <div className='font-semibold'>Notes:</div>
-            <p className='my-1 line-clamp-6'>{notes}</p>
-          </div>
+    <div
+      className='mt-2 justify-between overflow-hidden text-sm'
+      style={{
+        maxHeight: isOpen ? '100vh' : 0,
+        transition: 'max-height 0.3s ease-in-out'
+      }}
+    >
+      <div className='max-h-screen space-y-1'>
+        <div className='space-x-1'>
+          <span className='font-semibold'>Visit Type:</span>
+          <span>{type}</span>
         </div>
-        <div className='flex items-center justify-around py-2'>
-          <div className='w-2/5'>
-            <Button
-              size='small'
-              onClick={() => router.push(`/visit/${docId}/incident?${params}`)}
-            >
-              Report Incident
-            </Button>
-          </div>
-          <div className='w-2/5'>
-            <Button
-              size='small'
-              onClick={() => router.push(`/visit/${docId}/vet?${params}`)}
-            >
-              Register Vet Concern
-            </Button>
-          </div>
-          <div className='w-fit'>
-            <EditButton id={docId} />
-          </div>
+        <div className='space-x-1'>
+          <span className='font-semibold'>Pet(s):</span>
+          <span>{petNames}</span>
+        </div>
+        <div className='space-x-1'>
+          <span className='font-semibold'>Duration:</span>
+          <span>{formatDuration(duration)}</span>
+        </div>
+        <div className='space-x-1'>
+          <span className='font-semibold'>Walk Distance:</span>
+          <span>{walkDist.toFixed(3)} km</span>
+        </div>
+        <div className='space-x-1'>
+          <span className='font-semibold'>Commute Distance:</span>
+          <span>{commuteDist.toFixed(1)} km</span>
+        </div>
+        <div className='space-x-1'>
+          <span className='font-semibold'>Commute Method:</span>
+          <span>{commuteMethod}</span>
+        </div>
+        <div>
+          <div className='font-semibold'>Notes:</div>
+          <p className='my-1 line-clamp-6'>{notes}</p>
         </div>
       </div>
-    </>
+      <div className='flex items-center justify-around py-2'>
+        <div className='w-2/5'>
+          <Button
+            size='small'
+            onClick={() => router.push(`/visit/${docId}/incident?${params}`)}
+          >
+            Report Incident
+          </Button>
+        </div>
+        <div className='w-2/5'>
+          <Button
+            size='small'
+            onClick={() => router.push(`/visit/${docId}/vet?${params}`)}
+          >
+            Register Vet Concern
+          </Button>
+        </div>
+        <div className='w-fit'>
+          <EditButton id={docId} />
+        </div>
+      </div>
+    </div>
   )
 }
 
