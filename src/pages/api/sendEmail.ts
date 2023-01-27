4624aa3bec6fc18530bd912e785abc44963a13ed
Zach Manson <zachpmanson@gmail.com>
2023-01-27 23:36:44 +0800

    Formatting

--- src/pages/api/sendEmail.ts
+++ src/pages/api/sendEmail.ts
@@ -1,19 +1,20 @@
 import { NextApiRequest, NextApiResponse } from 'next'
 
-import { auth, firestore } from '@/lib/temp/firebase/admin/init'
+import { firestore } from '@/lib/temp/firebase/admin/init'
 
 interface Message {
   message: {
-    subject: string;
-    text: string;
-    html: string;
+    subject: string
+    text: string
+    html: string
   }
 }
 
 const handler = async (req: NextApiRequest, res: NextApiResponse) => {
   const email = {
     to: process.env.INCIDENT_EMAIL,
-    message: JSON.parse(req.body) as Message}
+    message: JSON.parse(req.body) as Message
+  }
   try {
     await firestore.collection('mail').add(email)
     res.status(200).send({ message: 'Success' })
