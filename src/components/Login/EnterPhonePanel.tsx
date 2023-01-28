9c0637da1911d6d2c8852a587a6a967c50da0986
Alison0816 <22835304@student.uwa.edu.au>
2023-01-28 16:06:42 +0800

    update phone number validation with library (need to chage style)

--- src/components/Login/EnterPhonePanel.tsx
+++ src/components/Login/EnterPhonePanel.tsx
@@ -1,69 +1,33 @@
-import { useState } from 'react'
+import React, { useState } from 'react'
+import PhoneInput from 'react-phone-number-input'
 
-import Form from '@/components/Login/LoginForm'
 import Button from '@/components/UI/button'
-import { SelectOption } from '@/types/types'
+
+import 'react-phone-number-input/style.css'
 
 export interface EnterPhonePanelInterface {
   onClick: (phoneNumber: string) => void
 }
 
-const EnterPhonePanel = ({ onClick }: EnterPhonePanelInterface) => {
-  const [displaynumber, setdisplaynumber] = useState('')
-  const [countrycode, setcountrycode] = useState('')
+const EnterPhonePanel = () => {
+  const [value, setValue] = useState<string>()
 
-  const countryCodeSelectOptions: SelectOption<string>[] = [
-    { value: '+61', label: 'Australia (+61)' }
-  ]
-
-  function handleSubmit(event: React.ChangeEvent<HTMLFormElement>) {
-    onClick(countrycode + displaynumber)
-    event.preventDefault()
-    event.target.reset()
-  }
   /* TODO: add 'auth update' functionality */
 
-  function formFilled() {
-    return displaynumber !== ''
-  }
+  console.log(value)
 
   return (
-    <div className='flex w-1/4 flex-auto flex-col'>
-      <form
-        onSubmit={handleSubmit}
-        className='m-5 flex h-full flex-auto flex-col items-center justify-around'
-      >
-        <div className='flex justify-center'>
-          <Form
-            id='countrycode'
-            label='Country Code'
-            type='select'
-            isNumPad={false}
-            placeholder={countrycode}
-            isRequired={true}
-            onChange={(event) => setcountrycode(event.target.value)}
-            selectOptions={countryCodeSelectOptions}
-          />
-          <Form
-            id='phonenumber'
-            label='Phone Number'
-            type='text'
-            isNumPad={true}
-            placeholder='  '
-            isRequired={true}
-            onChange={(event) => setdisplaynumber(event.target.value)}
-          />
-        </div>
-        <Button
-          className='w-fit'
-          size='large'
-          type='submit'
-          disabled={!formFilled()}
-        >
-          CONTINUE
-        </Button>
-      </form>
-    </div>
+    <form className='m-5 flex h-full flex-auto flex-col items-center justify-around'>
+      <PhoneInput
+        defaultCountry='AU'
+        placeholder='Enter phone number'
+        value={value}
+        onChange={setValue}
+      />
+      <Button className='w-fit' type='submit' disabled={value === undefined}>
+        CONTINUE
+      </Button>
+    </form>
   )
 }
 
