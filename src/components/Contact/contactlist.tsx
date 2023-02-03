431892155813da4c9610dd1b2bb61cfa6dd920a4
Techno-UWA <techno-uwa@outlook.com>
2023-02-03 15:16:01 +0800

    Added filter by tag atom to contact list

--- src/components/Contact/contactlist.tsx
+++ src/components/Contact/contactlist.tsx
@@ -1,18 +1,21 @@
 import { XCircleIcon } from '@heroicons/react/24/outline'
 import { useAtomValue } from 'jotai'
 
-import { searchQueryAtom } from '@/atoms/contacts'
+import { searchQueryAtom, searchTagsAtom } from '@/atoms/contacts'
 import ContactItem from '@/components/Contact/contactitem'
 import { useContacts } from '@/hooks/contacts'
 
 const ContactList = () => {
   const { data: contacts } = useContacts()
   const searchQuery = useAtomValue(searchQueryAtom)
+  const searchTags = useAtomValue(searchTagsAtom)
   if (contacts === undefined) return null
 
   const contactItems = contacts
-    .filter((contact) =>
-      contact.name.toLowerCase().includes(searchQuery.toLowerCase())
+    .filter(
+      (contact) =>
+        contact.name.toLowerCase().includes(searchQuery.toLowerCase()) &&
+        (searchTags === '' || contact.tags.includes(searchTags))
     )
     .sort((a, b) => a.name.localeCompare(b.name))
     .map((contact) => <ContactItem key={contact.docId} contact={contact} />)
