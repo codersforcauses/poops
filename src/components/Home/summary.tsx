b43b9eaa45d22d9db4113f6c2704c1d902b8b112
Dylan To <dylan.to@hotmail.com>
2023-02-16 20:10:24 +0800

    refac: remove unnecessary field control and refac styling fix

--- src/components/Home/summary.tsx
+++ src/components/Home/summary.tsx
@@ -23,21 +23,21 @@ function Summary() {
       <table className='w-full'>
         <tbody>
           <tr>
-            <td className='pr-2'>
+            <td>
               <Statistics
                 title='Number of Visits'
                 data={numVisits.toString()}
               />
             </td>
-            <td className='pl-2'>
+            <td>
               <Statistics title='Distance Walked' data={walkedDist + ' km'} />
             </td>
           </tr>
           <tr>
-            <td className='pr-2'>
+            <td>
               <Statistics title='Number of Hours' data={numHours.toString()} />
             </td>
-            <td className='pl-2'>
+            <td>
               <Statistics
                 title='Distance Commuted'
                 data={commutedDist + ' km'}
--- src/components/UI/FormComponents/PhoneField/index.tsx
+++ src/components/UI/FormComponents/PhoneField/index.tsx
@@ -1,4 +1,4 @@
-import { useContext, useEffect } from 'react'
+import { useContext } from 'react'
 import { CountryCode } from 'libphonenumber-js/min'
 import PhoneInputWithCountrySelect from 'react-phone-number-input/react-hook-form'
 
@@ -6,11 +6,7 @@ import {
   FormContext,
   FormFieldProps
 } from '@/components/UI/FormComponents/Form/context'
-import {
-  FieldControl,
-  FieldLabel,
-  FieldMessage
-} from '@/components/UI/FormComponents/utils'
+import { FieldLabel, FieldMessage } from '@/components/UI/FormComponents/utils'
 
 interface PhoneSelectProps extends FormFieldProps {
   isDisabled?: boolean
@@ -26,63 +22,36 @@ const PhoneField = ({
   isDisabled = false,
   required = false,
   rules = {},
-  setFocused,
   defaultCountry = 'AU',
   ...props
 }: PhoneSelectProps) => {
-  const {
-    formState,
-    disabled: formDisabled,
-    register,
-    watch,
-    setFocus
-  } = useContext(FormContext)
+  const { formState, disabled: formDisabled } = useContext(FormContext)
   const error: string | undefined =
     formState?.errors?.[name]?.message?.toString() || undefined
 
-  const value = watch?.(name)
-
-  useEffect(() => {
-    setFocused && setFocus?.(name)
-  }, [name, setFocus, setFocused])
-
-  useEffect(() => {
-    register?.(name)
-  }, [register, name])
-
-  // const handleChange = (value: string) => {
-  //   setValue?.(name, value)
-  // }
-
   return (
-    <FieldControl
-      name={name}
-      error={error}
-      required={'required' in rules || required}
-      disabled={formDisabled || isDisabled}
-    >
-      <div className='flex flex-col'>
-        <FieldLabel>{label}</FieldLabel>
-        <div
-          className={`form-input flex flex-row justify-center rounded border border-gray-500 bg-white p-0 ${props.className}`}
-        >
-          <PhoneInputWithCountrySelect
-            name={name}
-            defaultCountry={defaultCountry}
-            className='w-full'
-            disabled={isDisabled}
-            placeholder='0412 345 678'
-            value={value}
-            rules={rules}
-          />
-        </div>
-        {error ? (
-          <FieldMessage>{error}</FieldMessage>
-        ) : (
-          description && <FieldMessage description>{description}</FieldMessage>
-        )}
+    <div className='flex flex-col'>
+      <FieldLabel>{label}</FieldLabel>
+      <div
+        className={`form-input flex flex-row justify-center rounded border border-gray-500 bg-white p-0 ${props.className}`}
+      >
+        <PhoneInputWithCountrySelect
+          name={name}
+          defaultCountry={defaultCountry}
+          className='w-full'
+          disabled={formDisabled || isDisabled}
+          error={error}
+          required={'required' in rules || required}
+          placeholder='0412 345 678'
+          rules={rules}
+        />
       </div>
-    </FieldControl>
+      {error ? (
+        <FieldMessage>{error}</FieldMessage>
+      ) : (
+        description && <FieldMessage description>{description}</FieldMessage>
+      )}
+    </div>
   )
 }
 
