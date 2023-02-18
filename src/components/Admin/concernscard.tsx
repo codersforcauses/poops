78c971d243b5e2dec565eb1cce6eaa46b49c2fdd
dct <dylan.to@hotmail.com>
2023-02-18 14:56:34 +0800

    Le Buttone
    Co-authored-by: Justin Liang <justinliangg@users.noreply.github.com>

--- src/components/Admin/concernscard.tsx
+++ src/components/Admin/concernscard.tsx
@@ -1,24 +1,35 @@
+import Button from '@/components/UI/button'
 import { VetConcern } from '@/types/types'
 import { formatTimestamp } from '@/utils'
 
 const ConcernsCard = (props: VetConcern) => {
+  const handleResolve = () => {
+    // send request to resolve concern
+    // if successful, remove card from page, show success with option to undo?
+  }
+
   return (
-    <div className='m-2 flex flex-col rounded-xl bg-gray-50 p-4 shadow-lg'>
-      <div className='flex justify-between'>
-        <div className='font-bold'>
-          <p className='font-bold text-primary'>
-            {props.clientName} {formatTimestamp(props.visitTime)}
-          </p>
-          <p className='text-sm'>{props.userName}</p>
+    <div className='m-2 flex flex-col gap-2 rounded-xl bg-gray-50 p-4 shadow-lg'>
+      <div className='flex flex-col'>
+        <div className='flex justify-between'>
+          <div className='font-bold'>
+            <p className='font-bold text-primary'>
+              {props.clientName} {formatTimestamp(props.visitTime)}
+            </p>
+            <p className='text-sm'>{props.userName}</p>
+          </div>
         </div>
+        <div>{props.userEmail}</div>
+        <div>Report date: {formatTimestamp(props.createdAt)}</div>
+        <div>User Phone: {props.userPhone}</div>
+        <div>Visit ID: {props.visitId}</div>
+        <div>Client: {props.clientName}</div>
+        <div>Pet: {props.petName}</div>
+        <div>{props.detail}</div>
       </div>
-      <div>{props.userEmail}</div>
-      <div>Report date: {formatTimestamp(props.createdAt)}</div>
-      <div>User Phone: {props.userPhone}</div>
-      <div>Visit ID: {props.visitId}</div>
-      <div>Client: {props.clientName}</div>
-      <div>Pet: {props.petName}</div>
-      <div>{props.detail}</div>
+      <div className='flex justify-center'>
+        <Button onClick={handleResolve}>Mark as Resolved</Button>
+      </div>
     </div>
   )
 }
--- src/components/Admin/incidentcard.tsx
+++ src/components/Admin/incidentcard.tsx
@@ -1,23 +1,34 @@
+import Button from '@/components/UI/button'
 import { Incident } from '@/types/types'
 import { formatTimestamp } from '@/utils'
 
 const IncidentCard = (props: Incident) => {
+  const handleResolve = () => {
+    // send request to resolve incident
+    // if successful, remove card from page, show success with option to undo?
+  }
+
   return (
-    <div className='m-2 flex flex-col rounded-xl bg-gray-50 p-4 shadow-lg'>
-      <div className='flex justify-between'>
-        <div className='font-bold'>
-          <p className='font-bold text-primary'>
-            {props.clientName} {formatTimestamp(props.time)}
-          </p>
-          <p className='text-sm'>{props.userName}</p>
+    <div className='m-2 flex flex-col gap-2 rounded-xl bg-gray-50 p-4 shadow-lg'>
+      <div className='flex flex-col'>
+        <div className='flex justify-between'>
+          <div className='font-bold'>
+            <p className='font-bold text-primary'>
+              {props.clientName} {formatTimestamp(props.time)}
+            </p>
+            <p className='text-sm'>{props.userName}</p>
+          </div>
         </div>
+        <div>{props.email}</div>
+        <div>Report date: {formatTimestamp(props.createdAt)}</div>
+        <div>Visit ID: {props.visitId}</div>
+        <div>Client: {props.clientName}</div>
+        <div>Pet: {props.petName}</div>
+        <div>{props.details}</div>
       </div>
-      <div>{props.email}</div>
-      <div>Report date: {formatTimestamp(props.createdAt)}</div>
-      <div>Visit ID: {props.visitId}</div>
-      <div>Client: {props.clientName}</div>
-      <div>Pet: {props.petName}</div>
-      <div>{props.details}</div>
+      <div className='flex justify-center'>
+        <Button onClick={handleResolve}>Mark as Resolved</Button>
+      </div>
     </div>
   )
 }
