fbdc65e011e133cd1acab2d536c8c1e698dec292
HUIXIN-TW <huixin.yang.tw@gmail.com>
2023-02-19 16:24:57 +0800

    formatting

--- src/components/Admin/incidentcard.tsx
+++ src/components/Admin/incidentcard.tsx
@@ -7,15 +7,19 @@ const IncidentCard = (props: Incident) => {
   return (
     <div className='m-2 flex flex-col rounded-xl bg-gray-50 p-4 drop-shadow-default'>
       <div className='font-bold text-primary'>
-        {props.clientName} {moment(props.reportTime.toDate())
-              .format('YYYY-MM-DD HH:mm:ss')
-              .toString()}
+        {props.clientName}{' '}
+        {moment(props.reportTime.toDate())
+          .format('YYYY-MM-DD HH:mm:ss')
+          .toString()}
       </div>
       <div className='text-sm'>{props.userName}</div>
       <div>{props.userEmail}</div>
-      <div>Report date: {moment(props.reportTime.toDate())
-              .format('YYYY-MM-DD HH:mm:ss')
-              .toString()}</div>
+      <div>
+        Report date:{' '}
+        {moment(props.reportTime.toDate())
+          .format('YYYY-MM-DD HH:mm:ss')
+          .toString()}
+      </div>
       <div>Visit ID: {props.visitId}</div>
       <div>Client: {props.clientName}</div>
       <div>Pet: {props.petName}</div>
